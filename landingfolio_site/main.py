1ed9c77f6c17bee5b4c7986753e1ad2eccc8bdb9
maxexplorer <maxexplorer@mail.ru>
2022-06-07 22:31:53 +0300

    landingfolio_site_asyncio_aiohttp_aiofiles

--- landingfolio_site/main.py
+++ landingfolio_site/main.py
@@ -4,6 +4,7 @@ import os
 import time
 
 
+
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
@@ -70,7 +71,7 @@ def download_imgs(file_path):
     items_len = len(src)
     count = 1
 
-    for item in src[:10]:
+    for item in src[:100]:
         item_name = item.get("title")
         item_imgs = item.get("images")
 
