6ff4c1f901c10888d954f8c28f0cf06ae6efcbc1
maxexplorer <maxexplorer@mail.ru>
2023-01-02 19:23:56 +0300

    wildberries_site result_list.json

--- wildberries_site/main.py
+++ wildberries_site/main.py
@@ -4,10 +4,6 @@ import os
 import time
 import json
 
-# url = "https://static-basket-01.wb.ru/vol0/data/main-menu-ru-ru.json"
-url = "https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=3,6,19,21,8&curr=rub&dest=-1059500,-72639,-1754564,-4653095&emp=0&lang=ru&locale=ru&pricemarginCoeff=1.0&query=%D0%BE%D0%B4%D0%B5%D0%B6%D0%B4%D0%B0&reg=0&regions=80,64,83,4,38,33,86,30,40,48,1,66,31,68,22&resultset=catalog&sort=popular&spp=0&suppressSpellcheck=false"
-# url = "https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=3,6,19,21,8&curr=rub&dest=-1059500,-72639,-1754564,-4653095&emp=0&lang=ru&locale=ru&page=2&pricemarginCoeff=1.0&query=%D0%BE%D0%B4%D0%B5%D0%B6%D0%B4%D0%B0&reg=0&regions=80,64,83,4,38,33,86,30,40,48,1,66,31,68,22&resultset=catalog&sort=popular&spp=0&suppressSpellcheck=false"
-
 headers = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
@@ -15,20 +11,42 @@ headers = {
 }
 
 
-def get_data(url, headers):
-    response = requests.get(url=url, headers=headers)
+def get_data(headers):
+    result_list = []
 
-    data = response.json()
+    product = 'кофе'
 
-    if not os.path.exists('data'):
-        os.mkdir('data')
+    for i in range(1, 2):
+        page = i
+        url = f"https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=3,6,19,21,8&curr=rub&dest=-1059500,-72639,-1754564,-4653095&emp=0&lang=ru&locale=ru&{page=}&pricemarginCoeff=1.0&query={product}&reg=0&regions=80,64,83,4,38,33,86,30,40,48,1,66,31,68,22&resultset=catalog&sort=popular&spp=0&suppressSpellcheck=false"
 
-    with open('data/data_json.json', 'w', encoding='utf-8') as file:
-        json.dump(data, file, indent=4, ensure_ascii=False)
+        response = requests.get(url=url, headers=headers)
+
+        data = response.json()
+        items = data['data']['products']
+
+        for item in items:
+            result_list.append(
+                {
+                    'title': item.get('name'),
+                    'brand': item.get('brand'),
+                    'sale': item.get('sale'),
+                    'price': item.get('priceU') // 100,
+                    'sale_price': item.get('salePriceU') // 100,
+                    'rating': item.get('rating')
+
+                }
+            )
+
+        if not os.path.exists('data'):
+            os.mkdir('data')
+
+        with open('data/result_list.json', 'w', encoding='utf-8') as file:
+            json.dump(result_list, file, indent=4, ensure_ascii=False)
 
 
 def main():
-    get_data(url=url, headers=headers)
+    get_data(headers=headers)
 
 
 if __name__ == '__main__':
