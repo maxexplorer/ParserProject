8a8bb881c431f58f9fe215209327d8b59b5bd42d
maxexplorer <maxexplorer@mail.ru>
2023-02-04 18:11:52 +0300

    avito_site get_html

--- avito_site/main.py
+++ avito_site/main.py
@@ -5,8 +5,6 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import os
 
-url = "https://www.avito.ru/all/odezhda_obuv_aksessuary/obuv_muzhskaya-ASgBAgICAUTeArqp1gI?cd=1"
-
 
 def get_html(url):
     options = Options()
@@ -23,29 +21,17 @@ def get_html(url):
 
     try:
         browser.get(url=url)
-        print(browser.window_handles)
-        print(browser.current_url)
+
         # time.sleep(5)
         browser.implicitly_wait(5)
 
-        items = browser.find_elements(By.CSS_SELECTOR, '[data-marker=item]')
-        items[0].click()
-        # time.sleep(5)
-
-        browser.switch_to.window(browser.window_handles[1])
-        print(browser.current_url)
-
-
-
-        # html = browser.page_source
-        # return html
+        html = browser.page_source
+        return html
     except Exception as ex:
         print(ex)
     finally:
         browser.close()
         browser.quit()
-        time.sleep(5)
-
 
 def get_content(html):
     soup = BeautifulSoup(html, 'lxml')
@@ -55,16 +41,23 @@ def get_content(html):
     for item in items:
         title = item.find('h3', itemprop='name')
         print(title.text)
+    return items
 
 
 def main():
-    html = get_html(url=url)
-    # if not os.path.exists('data'):
-    #     os.mkdir('data')
-    # with open('data/index.html',  'w', encoding='utf-8') as file:
-    #     file.write(html)
-    # with open('data/index.html', 'r', encoding='utf-8') as file:
-    #     html = file.read()
+    # pages = int(input('Введите количество страниц: '))
+    # brand = 'samsung'
+    # for page in range(1, pages + 1):
+    #     url = f"https://www.avito.ru/all/telefony/mobilnye_telefony/samsung-ASgBAgICAkS0wA2crzmwwQ2I_Dc?cd=1&p={page}&q={brand}"
+    #     html = get_html(url=url)
+    #     if not os.path.exists('data'):
+    #         os.mkdir('data')
+    #     with open('data/index.html', 'w', encoding='utf-8') as file:
+    #         file.write(html)
+    with open('data/index.html', 'r', encoding='utf-8') as file:
+        html = file.read()
+    cards = get_content(html)
+    print(len(cards))
     # print(get_content(html))
 
 
