9af9820d1834c278a808e2902e6d53042101a9f6
maxexplorer <maxexplorer@mail.ru>
2023-02-11 19:53:19 +0300

    avito_site get_content get_json

--- avito_site/main.py
+++ avito_site/main.py
@@ -4,14 +4,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import os
+import json
 
 
 def get_html(url):
     options = Options()
     options.add_argument('User-Agent = Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
                          ' Chrome/108.0.0.0 Safari/537.36')
-    # options.add_argument("--disable-blink-features=AutomationControlled")
-    # options.add_argument("--headless")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--headless")
 
     browser = webdriver.Chrome(
         executable_path="C:/Users/Макс/PycharmProjects/ParserProject/chromedriver/chromedriver.exe",
@@ -44,46 +45,66 @@ def get_content(html):
         try:
             title = item.find('h3', itemprop='name').text.strip()
         except Exception:
-            title = 'Нет названия'
+            title = None
         try:
             price = int(item.find('span', itemprop='offers').text.strip().replace('\xa0', '').replace('₽', ''))
         except Exception:
-            price = ''
+            price = None
         try:
-            params = item.find('div', {'data-marker': 'item-specific-params'}).text.strip()
+            params = item.find('div', {'data-marker': 'item-specific-params'}).text.strip().replace('\xa0', '')
         except Exception:
-            params = ''
+            params = None
         try:
-            description = item.find('div', class_='iva-item-descriptionStep-C0ty1').text.strip()
+            description = item.find('div', class_='iva-item-descriptionStep-C0ty1').text.strip().replace('\n', '')
         except Exception:
-            description = ''
+            description = None
         try:
             address = item.find('div', class_='geo-address-fhHd0 text-text-LurtD text-size-s-BxGpL').text.strip()
         except Exception:
-            address = ''
+            address = None
         try:
             url = 'https://www.avito.ru/' + item.find('a').get('href')
         except Exception:
-            url = ''
-        print(title, price, params, description, address, url)
-    return items
+            url = None
+
+        cards.append(
+            {
+                'title': title,
+                'price': price,
+                'params': params,
+                'description': description,
+                'address': address,
+                'url': url
+            }
+        )
+    return cards
+
+
+def save_json(data):
+    if not os.path.exists('data'):
+        os.mkdir('data')
+
+    with open('data/data.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
 
 
 def main():
-    # pages = int(input('Введите количество страниц: '))
-    # brand = 'samsung'
-    # for page in range(1, pages + 1):
-    #     url = f"https://www.avito.ru/all/telefony/mobilnye_telefony/samsung-ASgBAgICAkS0wA2crzmwwQ2I_Dc?cd=1&p={page}&q={brand}"
-    #     html = get_html(url=url)
+    pages = int(input('Введите количество страниц: '))
+    brand = 'samsung'
+    for page in range(1, pages + 1):
+        url = f"https://www.avito.ru/all/telefony/mobilnye_telefony/samsung-ASgBAgICAkS0wA2crzmwwQ2I_Dc?cd=1&p={page}&q={brand}"
+        html = get_html(url=url)
+        cards = get_content(html)
+        save_json(cards)
     #     if not os.path.exists('data'):
     #         os.mkdir('data')
     #     with open('data/index.html', 'w', encoding='utf-8') as file:
     #         file.write(html)
-    with open('data/index.html', 'r', encoding='utf-8') as file:
-        html = file.read()
-    cards = get_content(html)
-    print(len(cards))
-    # print(get_content(html))
+    # with open('data/index.html', 'r', encoding='utf-8') as file:
+    #     html = file.read()
+    # cards = get_content(html)
+    # print(len(cards))
+    # # print(get_content(html))
 
 
 if __name__ == '__main__':
