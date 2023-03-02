e9f7608e64189f8a9766335619ca770b00d61d94
maxexplorer <maxexplorer@mail.ru>
2023-03-02 14:14:47 +0300

    csmoney_site def get_data

--- csmoney_site/main.py
+++ csmoney_site/main.py
@@ -1,18 +1,26 @@
 import requests
 from fake_useragent import UserAgent
+import json
+import os
 
 useragent = UserAgent()
 
-url = "https://cs.money/ru/market/buy/"
+# url = "https://cs.money/ru/market/buy/"
+url = "https://cs.money/1.0/market/sell-orders?limit=60&offset=0&type=5"
 
 headers = {
-    'User-Agent': useragent
+    'User-Agent': useragent.random
 }
 
 
 def get_data(url, headers):
     response = requests.get(url=url, headers=headers)
-    print(response.status_code)
+
+    if not os.path.exists('data'):
+        os.mkdir('data')
+
+    with open('data/data.json', 'w', encoding='utf-8') as file:
+        json.dump(response.json(), file, indent=4, ensure_ascii=False)
 
 
 def main():
