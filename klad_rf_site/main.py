ebba48ef5f18d1c52f37155d4464cc7d950d1732
maxexplorer <maxexplorer@mail.ru>
2023-03-05 21:36:35 +0300

    klad_rf_site

--- klad_rf_site/main.py
+++ klad_rf_site/main.py
@@ -17,11 +17,11 @@ headers = {
 def get_html(url, headers):
     response = requests.get(url=url, headers=headers)
 
-    if not os.path.exists('data'):
-        os.mkdir('data')
-
-    with open('data/index.html', 'w', encoding='utf-8') as file:
-        file.write(response.text)
+    # if not os.path.exists('data'):
+    #     os.mkdir('data')
+    #
+    # with open('data/index.html', 'w', encoding='utf-8') as file:
+    #     file.write(response.text)
 
     html = response.text
     return html
@@ -39,23 +39,56 @@ def get_data(html):
             continue
         try:
             url = "https://kladr-rf.ru" + item.find('a').get('href')
-            city = item.text.strip().strip('Город')
+            city = item.text.strip().strip()
             print(city)
-            html = get_html(url=url, headers=headers)
-            soup = BeautifulSoup(html, 'lxml')
+            html_city = get_html(url=url, headers=headers)
+            soup = BeautifulSoup(html_city, 'lxml')
             streets = soup.find(class_='pb-2 fs-5 fw-bold border-bottom', text='Улицы').find_next().find_all('li')
             for street in streets:
-                if 'class="old"' in str(street):
+                if 'class="old"' in str(item) or 'class="del"' in str(item):
                     continue
                 city_streets.append(
                     {
 
                         'Город': city,
-                        'Улица': street.text
+                        'Улица': street.text.strip()
                     }
                 )
         except Exception:
             continue
+    soup = BeautifulSoup(html, 'lxml')
+    items = soup.find('div', class_='pb-2 fs-5 fw-bold border-bottom', text='Районы').find_next().find_all('li')
+
+    for item in items:
+        if 'class="old"' in str(item) or 'class="del"' in str(item):
+            continue
+        try:
+            url = "https://kladr-rf.ru" + item.find('a').get('href')
+            html_district = get_html(url=url, headers=headers)
+            soup = BeautifulSoup(html_district, 'lxml')
+            items = soup.find('div', class_='pb-2 fs-5 fw-bold border-bottom', text='Города').find_next().find_all('li')
+            for item in items:
+                if 'class="old"' in str(item) or 'class="del"' in str(item):
+                    continue
+                url = "https://kladr-rf.ru" + item.find('a').get('href')
+                city = item.text.strip().strip()
+                print(city)
+                html_city = get_html(url=url, headers=headers)
+                soup = BeautifulSoup(html_city, 'lxml')
+                streets = soup.find(class_='pb-2 fs-5 fw-bold border-bottom', text='Улицы').find_next().find_all('li')
+                for street in streets:
+                    if 'class="old"' in str(item) or 'class="del"' in str(item):
+                        continue
+                    city_streets.append(
+                        {
+
+                            'Город': city,
+                            'Улица': street.text.strip()
+                        }
+                    )
+        except Exception:
+            continue
+
     return city_streets
 
 
@@ -65,6 +98,8 @@ def save_json(data):
 
     with open('data/data.json', 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
+
+    print()
 
 
 def save_excel(data):
@@ -81,7 +116,7 @@ def save_excel(data):
 
 def main():
     result_list = []
-    for i in range(1, 89):
+    for i in range(1, 90):
         if i in (80, 81, 82, 84, 85, 88):
             continue
         url = f"https://kladr-rf.ru/{str(i).zfill(2)}/"
