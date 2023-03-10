bc5677352aab51a3fffaa6fcc52089f8eb478393
maxexplorer <maxexplorer@mail.ru>
2023-03-10 18:47:30 +0300

    mnogosna_site add def get_pages()

--- mnogosna_site/main.py
+++ mnogosna_site/main.py
@@ -48,16 +48,46 @@ def get_data(html):
     items = soup.find_all('div', class_='col-sm-6 col-md-4 col-lg-3')
 
     for item in items:
-        title = item.find('div', class_='p-card__name').text.strip()
-        rating = item.find('span', class_='pc-rating__value').text.strip()
-        status = item.find('div', class_='status status--grid status--available').text.strip()
-        dimensions = item.find('div', class_='select select--xs').text.strip()
-        # price = ''.join(i for i in item.find('div', class_='pc-price__value js-price').text.strip() if i.isdigit())
-        # is equivalent to
-        price = ''.join(filter(lambda x: x.isdigit(), item.find('div', class_='pc-price__value js-price').text.strip()))
+        try:
+            title = item.find('div', class_='p-card__name').text.strip()
+        except Exception:
+            title = None
+        try:
+            rating = item.find('span', class_='pc-rating__value').text.strip()
+        except:
+            rating = None
+        try:
+            status = item.find('div', class_='status status--grid status--available').text.strip()
+        except Exception:
+            status = None
+        try:
+            dimensions = item.find('div', class_='select select--xs').text.strip()
+        except Exception:
+            dimensions = None
+        try:
+            # price = ''.join(i for i in item.find('div', class_='pc-price__value js-price').text.strip() if i.isdigit())
+            # is equivalent to
+            price = ''.join(filter(lambda x: x.isdigit(), item.find('div', class_='pc-price__value js-price').text.strip()))
+        except Exception:
+            price = None
+        try:
+            old_price = ''.join(filter(lambda x: x.isdigit(), item.find('div', class_='pc-price__old').text.strip()))
+        except Exception:
+            old_price = None
+        try:
+            discount = item.find('div', class_='pc-price__discount js-price-discount-percent').text.strip('-')
+        except Exception:
+            discount = None
+        try:
+            description = item.find('div', class_='pc-descr').text.strip()
+        except Exception:
+            description = None
+        try:
+            url = "https://mnogosna.ru" + item.find('div', class_='p-card__name').find('a').get('href')
+        except Exception:
+            url = None
 
-
-        print(f"{title}|||{rating} ||| {status} ||| {dimensions} ||| {price}")
+        print(f"{title}|||{rating} ||| {status} ||| {dimensions} ||| {price} ||| {old_price} ||| {discount} ||| {description} ||| {url}")
 
 
 
