668a16c7bfc1d4120532a067a8fb4a0ddf9832ab
maxexplorer <maxexplorer@mail.ru>
2023-03-12 12:37:02 +0300

    selenium_cloudflare

---
+++ parser_test/selenium_cloudflare.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import time
+
+# options = webdriver.ChromeOptions()
+# is equivalent to
+options = Options()
+options.add_argument("--disable-blink-features=AutomationControlled")
+# options.add_experimental_option("excludeSwitches", ["enable-automation"])
+# options.add_experimental_option('useAutomationExtension', False)
+
+service = Service(executable_path="C:/Users/Макс/PycharmProjects/ParserProject/chromedriver/chromedriver.exe")
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    'source': '''
+        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Array;
+        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Promise;
+        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Symbol;
+  '''
+})
+
+try:
+    driver.maximize_window()
+    driver.get(url="https://www.ozon.ru/")
+    time.sleep(10)
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
