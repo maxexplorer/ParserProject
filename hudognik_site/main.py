5d4fa78b65a642bdc64ead75d3fac531121a9b4c
maxexplorer <maxexplorer@mail.ru>
2023-03-19 18:35:57 +0300

    hudognik_site feat: def get_url()

--- hudognik_site/main.py
+++ hudognik_site/main.py
@@ -7,26 +7,50 @@ import csv
 
 useragent = UserAgent()
 
+url = "https://hudognik.net/artists/"
+
 headers = {
     'accept': '*/*',
     'user-agent': useragent.random
 }
 
 
-def get_url(headers):
-    url = "https://hudognik.net/artists/"
+def get_url(session, url, headers):
+    try:
+        response = session.get(url=url, headers=headers)
+        soup = BeautifulSoup(response.text, 'lxml')
+        pages_count = int(soup.find('div', class_='page_list page_list_hudognik').find_all('a')[-1].text.strip())
+    except Exception as ex:
+        print(ex)
+        pages_count = 1
 
-    response = requests.get(url=url, headers=headers)
+    url_list = []
 
-    if not os.path.exists('data'):
-        os.mkdir('data')
-
-    with open('data/index.html', 'w', encoding='utf-8') as file:
-        file.write(response.text)
+    # for page in range(1, pages_count + 1):
+    for page in range(1, 2):
+        try:
+            url = f"https://hudognik.net/artists/page{page}"
+            response = session.get(url=url, headers=headers)
+            soup = BeautifulSoup(response.text, 'lxml')
+        except Exception as ex:
+            print(ex)
+            continue
+        try:
+            page_urls = soup.find_all(itemprop='url')
+            for item in page_urls:
+                url = "https://hudognik.net" + item.get('href')
+                url_list.append(url)
+            print(url_list)
+        except Exception as ex:
+            print(ex)
+            continue
+    with open('data/url_list.txt', 'w', encoding='utf-8') as file:
+        print(*url_list, file=file, sep='\n')
 
 
 def main():
-    get_url(headers=headers)
+    with requests.Session() as session:
+        get_url(session=session, url=url, headers=headers)
 
 
 if __name__ == '__main__':
