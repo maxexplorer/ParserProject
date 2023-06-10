c3b0dc07bf6639d2c443a90248aee8c8f6aa272d
maximexplorer <maxexplorer@mail.ru>
2023-06-10 13:42:30 +0300

    kad_arbitr_site: refactor

--- kad_arbitr_site/main.py
+++ kad_arbitr_site/main.py
@@ -1,14 +1,21 @@
+import os
+import time
+from datetime import datetime
+import csv
+from random import randrange
+
 import requests
 from bs4 import BeautifulSoup
-import csv
-import os
-from datetime import datetime
-import pandas as pd
 from pandas import DataFrame, ExcelWriter
 import xlsxwriter
 
+start_time = datetime.now()
+
 
 def get_data(data_list):
+
+    cur_date = datetime.now().strftime('%d-%m-%Y')
+
     cookies = {
         '__ddg1_': 'MFMXvDe9vnfJ6yyzwrhA',
         'ASP.NET_SessionId': '2tbhmmracsoxnyzj0hv3nqij',
@@ -20,11 +27,11 @@ def get_data(data_list):
         'pr_fp': 'b8b6c4acd80151855d63363388cf302ae16a2b028cc87c7de3b2f8c6239c4650',
         'tmr_lvid': 'f106a92d04e109ac6cd53242d170122c',
         'tmr_lvidTS': '1686207226755',
-        '_ym_isad': '2',
         'rcid': 'fb3c3667-e7ec-41a8-9243-0b501e80bff3',
-        'tmr_detect': '0%7C1686249439775',
-        'wasm': 'd98631d33fda423afaf059f36f1d8dbe',
-        '_gat': '1',
+        '_ym_isad': '2',
+        'KadLVCards': '%d0%9051-15519%2f2022~%d0%9045-35689%2f2022~%d0%9035-2799%2f2023',
+        'tmr_detect': '0%7C1686288129628',
+        'wasm': '1b35952923aa821f9431b9e9ce78f0c0',
     }
 
     headers = {
@@ -32,7 +39,7 @@ def get_data(data_list):
         'accept': '*/*',
         'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
         'content-type': 'application/json',
-        # 'cookie': '__ddg1_=MFMXvDe9vnfJ6yyzwrhA; ASP.NET_SessionId=2tbhmmracsoxnyzj0hv3nqij; CUID=0e4e384b-57f0-429d-ab85-d3725a50463b:lXvk7RX5VuP9bMqQEgNZaQ==; _ga=GA1.2.1343078113.1686207225; _gid=GA1.2.6857850.1686207225; _ym_uid=1686207227142264491; _ym_d=1686207227; pr_fp=b8b6c4acd80151855d63363388cf302ae16a2b028cc87c7de3b2f8c6239c4650; tmr_lvid=f106a92d04e109ac6cd53242d170122c; tmr_lvidTS=1686207226755; _ym_isad=2; rcid=fb3c3667-e7ec-41a8-9243-0b501e80bff3; tmr_detect=0%7C1686249439775; wasm=d98631d33fda423afaf059f36f1d8dbe; _gat=1',
+        # 'cookie': '__ddg1_=MFMXvDe9vnfJ6yyzwrhA; ASP.NET_SessionId=2tbhmmracsoxnyzj0hv3nqij; CUID=0e4e384b-57f0-429d-ab85-d3725a50463b:lXvk7RX5VuP9bMqQEgNZaQ==; _ga=GA1.2.1343078113.1686207225; _gid=GA1.2.6857850.1686207225; _ym_uid=1686207227142264491; _ym_d=1686207227; pr_fp=b8b6c4acd80151855d63363388cf302ae16a2b028cc87c7de3b2f8c6239c4650; tmr_lvid=f106a92d04e109ac6cd53242d170122c; tmr_lvidTS=1686207226755; rcid=fb3c3667-e7ec-41a8-9243-0b501e80bff3; _ym_isad=2; KadLVCards=%d0%9051-15519%2f2022~%d0%9045-35689%2f2022~%d0%9035-2799%2f2023; tmr_detect=0%7C1686288129628; wasm=1b35952923aa821f9431b9e9ce78f0c0',
         'origin': 'https://kad.arbitr.ru',
         'referer': 'https://kad.arbitr.ru/',
         'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
@@ -46,14 +53,14 @@ def get_data(data_list):
         'x-requested-with': 'XMLHttpRequest',
     }
 
-    result_list = []
-
     with requests.Session() as session:
-        for i, tin, full_name, address, date, num_case in data_list[1:2]:
-            if tin.strip():
+        for i, tin, full_name, address, _, _ in data_list[1:]:
+            tin = tin.strip()
+            full_name = full_name.strip()
+            if tin:
                 name = tin
             else:
-                name = full_name.strip()
+                name = full_name
             json_data = {
                 'Page': 1,
                 'Count': 25,
@@ -72,53 +79,63 @@ def get_data(data_list):
                 'CaseNumbers': [],
                 'WithVKSInstances': False,
             }
+
+            session.headers.update(headers)
+            session.cookies.update(cookies)
+
+            time.sleep(randrange(3, 5))
+
             try:
                 response = session.post('https://kad.arbitr.ru/Kad/SearchInstances', cookies=cookies, headers=headers,
-                                        json=json_data)
+                                        json=json_data, timeout=30)
                 soup = BeautifulSoup(response.text, 'lxml')
+                print(response)
             except Exception as ex:
                 print(ex)
                 continue
-
+            print(f'Обрабатывается: {i}/{len(data_list)}')
             try:
                 date = soup.find('span', class_='js-rollover b-newRollover').text.strip()
             except Exception:
-                continue
+                date = None
             try:
                 num_case = soup.find('a', class_='num_case').text.strip()
             except Exception:
+                num_case = None
+            if date is None and num_case is None:
+                print(f'{full_name}: нет данных')
                 continue
 
-            result_list.append(
-                {
-                    'ИНН': tin,
-                    'ФИО': full_name,
-                    'Адрес собственника': address,
-                    'Дата': date,
-                    '№ дела': num_case
-                }
-            )
-    return result_list
 
 
-def save_excel(data):
-    if not os.path.exists('data'):
-        os.mkdir('data')
+            with open(f'data/data_{cur_date}.csv', 'a', encoding='utf-8') as file:
+                writer = csv.writer(file)
+                writer.writerow(
+                    (
+                        tin,
+                        full_name,
+                        address,
+                        date,
+                        num_case
+                    )
+                )
 
-    dataframe = DataFrame(data)
+    print(f'Данные сохранены в файл "data.csv"')
 
-    with ExcelWriter('data/output_data.xlsx', mode='w') as writer:
-        dataframe.to_excel(writer, sheet_name='data')
 
-    print(f'Данные сохранены в файл "data.xlsx"')
+
+
 
 
 def main():
     with open('data/input_data.csv', 'r', encoding='cp1251') as file:
         reader = csv.reader(file, delimiter=';')
         data_list = list(reader)
-    result_list = get_data(data_list=data_list)
-    save_excel(result_list)
+    print(f'Всего: {len(data_list)}')
+    get_data(data_list=data_list)
+    execution_time = datetime.now() - start_time
+    print('Сбор данных завершен!')
+    print(f'Время работы программы: {execution_time}')
 
 
 if __name__ == '__main__':
