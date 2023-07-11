b188144768e62308c2072554e1fa151fc6862bc2
maximexplorer <maxexplorer@mail.ru>
2023-07-11 14:10:34 +0300

    kad_arbitr_site

--- kad_arbitr_site/main.py
+++ kad_arbitr_site/main.py
@@ -44,14 +44,14 @@ def get_data(data_list):
         'rcid': '05df19e5-f8a9-499b-909d-dbc2f2d16af4',
         '__ddgid_': 'gEZsaD1SqQ2cWrLj',
         '__ddg2_': 'ieddKCUsnIp2Eavn',
-        '_gid': 'GA1.2.1459596806.1688723394',
+        '_gid': 'GA1.2.1412428495.1689073763',
         '_gat': '1',
         '_gat_FrontEndTracker': '1',
         '_dc_gtm_UA-157906562-1': '1',
-        '_ga_Q2V7P901XE': 'GS1.2.1688723394.5.0.1688723394.0.0.0',
+        '_ga_Q2V7P901XE': 'GS1.2.1689073762.6.0.1689073762.0.0.0',
         '_ym_isad': '2',
-        'tmr_detect': '0%7C1688723397244',
-        'wasm': 'a52c1a768462fea24becb88d4944661c',
+        'tmr_detect': '0%7C1689073765433',
+        'wasm': '39724cf66abfd76f6a72a03308731264',
     }
 
     headers = {
@@ -59,7 +59,7 @@ def get_data(data_list):
         'accept': '*/*',
         'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
         'content-type': 'application/json',
-        # 'cookie': '__ddg1_=hJ2gus2Uk92lVa8Ah0I1; ASP.NET_SessionId=d0afrxkozgstybxowcc305pw; CUID=612d9663-2b2e-4e58-b738-e4d1375b6f03:xQO0mrvJC121L36Lxa2Eew==; _ga=GA1.2.514976819.1686401335; pr_fp=b8b6c4acd80151855d63363388cf302ae16a2b028cc87c7de3b2f8c6239c4650; tmr_lvid=9e02cdea92d0e9183c217e306b5a8a19; tmr_lvidTS=1686401337221; _ym_uid=1686401338199628375; _ym_d=1686401338; rcid=05df19e5-f8a9-499b-909d-dbc2f2d16af4; __ddgid_=gEZsaD1SqQ2cWrLj; __ddg2_=ieddKCUsnIp2Eavn; _gid=GA1.2.1459596806.1688723394; _gat=1; _gat_FrontEndTracker=1; _dc_gtm_UA-157906562-1=1; _ga_Q2V7P901XE=GS1.2.1688723394.5.0.1688723394.0.0.0; _ym_isad=2; tmr_detect=0%7C1688723397244; wasm=a52c1a768462fea24becb88d4944661c',
+        # 'cookie': '__ddg1_=hJ2gus2Uk92lVa8Ah0I1; ASP.NET_SessionId=d0afrxkozgstybxowcc305pw; CUID=612d9663-2b2e-4e58-b738-e4d1375b6f03:xQO0mrvJC121L36Lxa2Eew==; _ga=GA1.2.514976819.1686401335; pr_fp=b8b6c4acd80151855d63363388cf302ae16a2b028cc87c7de3b2f8c6239c4650; tmr_lvid=9e02cdea92d0e9183c217e306b5a8a19; tmr_lvidTS=1686401337221; _ym_uid=1686401338199628375; _ym_d=1686401338; rcid=05df19e5-f8a9-499b-909d-dbc2f2d16af4; __ddgid_=gEZsaD1SqQ2cWrLj; __ddg2_=ieddKCUsnIp2Eavn; _gid=GA1.2.1412428495.1689073763; _gat=1; _gat_FrontEndTracker=1; _dc_gtm_UA-157906562-1=1; _ga_Q2V7P901XE=GS1.2.1689073762.6.0.1689073762.0.0.0; _ym_isad=2; tmr_detect=0%7C1689073765433; wasm=39724cf66abfd76f6a72a03308731264',
         'origin': 'https://kad.arbitr.ru',
         'referer': 'https://kad.arbitr.ru/',
         'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
@@ -72,11 +72,12 @@ def get_data(data_list):
         'x-date-format': 'iso',
         'x-requested-with': 'XMLHttpRequest',
     }
+
     with requests.Session() as session:
         if not os.path.exists('data'):
             os.mkdir('data')
 
-        for i, tin, full_name, address in data_list[500:700]:
+        for i, tin, full_name, address in data_list[673:1000]:
             tin = tin.strip()
             full_name = full_name.strip()
             if tin:
