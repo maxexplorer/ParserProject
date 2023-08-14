ec42fc70a7b51f646b8e3bad109f13cf30163aa8
maxexplorer <maxexplorer@mail.ru>
2023-08-14 13:49:09 +0300

    avito_site

--- avito_site/main.py
+++ avito_site/main.py
@@ -84,7 +84,7 @@ def save_json(data):
     if not os.path.exists('data'):
         os.mkdir('data')
 
-    with open('data/data.json', 'w', encoding='utf-8') as file:
+    with open('data/data.json', 'a', encoding='utf-8') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
 
@@ -96,16 +96,6 @@ def main():
         html = get_html(url=url)
         cards = get_content(html)
         save_json(cards)
-        if not os.path.exists('data'):
-            os.mkdir('data')
-        with open('data/index.html', 'w', encoding='utf-8') as file:
-            file.write(html)
-    with open('data/index.html', 'r', encoding='utf-8') as file:
-        html = file.read()
-    cards = get_content(html)
-    print(len(cards))
-    print(get_content(html))
-
 
 if __name__ == '__main__':
     main()
