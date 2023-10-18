2e09516595ccb5e8f690848a896992858fef4ee7
maxbaranov <maxexplorer@mail.ru>
2023-10-18 14:21:07 +0300

    temp_email: initial commit

---
+++ temp_email/main.py
@@ -0,0 +1,25 @@
+import requests
+import random
+import string
+import time
+import os
+
+
+def generate_username():
+    pass
+
+
+def check_mail():
+    pass
+
+
+def delete_mail():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
