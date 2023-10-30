7f398d5e470f815cbb1851414b77f9f65fe5bcf2
maxbaranov <maxexplorer@mail.ru>
2023-10-30 12:30:49 +0300

    dns_site: add def get_html()

--- dns_site/main.py
+++ dns_site/main.py
@@ -1,13 +1,25 @@
 import requests
 import os
 
+url = "url"
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+                  ' (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14931'
+}
 
-def get_html():
-    pass
+def get_html(url, headers, session):
+    try:
+        response = session.get(url=url, headers=headers, timeout=60)
+        html = response.text
+        return html
+    except Exception as ex:
+        print(ex)
 
 
 def main():
-    get_html()
+    session = requests.Session()
+    get_html(url=url, headers=headers, session=session)
 
 if __name__ == '__main__':
     main()
