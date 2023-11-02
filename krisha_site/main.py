cf9731940dda7a6b4b9096f4c7c51bd192ff117b
maxbaranov <maxexplorer@mail.ru>
2023-11-02 11:48:35 +0300

    krisha_site: add def get_data()

--- krisha_site/main.py
+++ krisha_site/main.py
@@ -1,10 +1,17 @@
 import requests
 import os
 from bs4 import BeautifulSoup
-
+from datetime import datetime
+from pandas import DataFrame, ExcelWriter
+import openpyxl
 
 url = "https://krisha.kz/prodazha/kvartiry/?das[house.year][to]=2023&das[price][from]=90000000&das[who]=1"
 
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+                  ' (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14931'
+}
 
 
 def get_html(url, headers, session):
@@ -19,14 +26,36 @@ def get_html(url, headers, session):
 def get_pages(html):
     soup = BeautifulSoup(html, 'lxml')
     try:
-        pages = int(soup.find('div', class_='pages').find_all('a')[-1].get('href').split('=')[-1])
-    except Exception:
-        pages = 1
-    return pages
+        pages = int(soup.find_all('a', class_='paginator__btn')[-2].text.strip())
+        return pages
+    except Exception as ex:
+        print(ex)
+
+
+def get_data(session, pages):
+    for i in range(1, pages + 1)[:1]:
+        url = f"https://krisha.kz/prodazha/kvartiry/?das[house.year][to]=2023&das[price][from]=90000000&das[who]=" \
+              f"1&page={i}"
+
+        html = get_html(url=url, headers=headers, session=session)
+
+        soup = BeautifulSoup(html, 'lxml')
+
+        try:
+            data = soup.select('div[data-list-id=main]')
+            print(data)
+        except Exception as ex:
+            print(ex)
+
+
 
 
 def main():
-    pass
+    with requests.Session() as session:
+        html = get_html(url=url, headers=headers, session=session)
+        pages = get_pages(html)
+        print(f'Total: {pages} pages')
+        data = get_data(session=session, pages=pages)
 
 
 if __name__ == '__main__':
