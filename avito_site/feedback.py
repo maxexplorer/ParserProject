2f447540871ea70fabbc68deba66d336682094cf
maxbaranov <maxexplorer@mail.ru>
2023-11-04 10:21:16 +0300

    avito_site/feedback.py: refactor

--- avito_site/feedback.py
+++ avito_site/feedback.py
@@ -1,4 +1,9 @@
 import requests
+from fake_useragent import UserAgent
+
+useragent = UserAgent()
+
+print(useragent.random)
 
 
 
@@ -68,7 +73,7 @@ headers = {
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+    'user-agent': useragent.random
 }
 
 params = {
@@ -82,3 +87,4 @@ response = requests.get(
     headers=headers,
 )
 
+print(response.json())
