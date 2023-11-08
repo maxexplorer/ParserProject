308f717427d949b69cb6b56878d0cbdda9e010b0
maxbaranov <maxexplorer@mail.ru>
2023-11-08 11:02:34 +0300

    hh_site: add def get_data()

--- hh_site/main.py
+++ hh_site/main.py
@@ -2,17 +2,48 @@ import json
 import os
 
 import requests
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
 
 
-# url = "https://api.hh.ru/areas/"
-url = "https://api.hh.ru/vacancies/"
+def get_urls():
+    pass
 
 
-response = requests.get(url=url)
+def get_data():
+    useragent = UserAgent()
 
-if not os.path.exists('data'):
-    os.mkdir('data')
+    text = 'Административный директор'
 
-with open('data/hh_data.json', 'w', encoding='utf-8') as file:
-    json.dump(response.json(), file, indent=4, ensure_ascii=False)
+    page = 10
 
+    url = f"https://hh.ru/search/resume?text={text}&area=1&isDefaultArea=true&exp_period=all_time&logic=normal&" \
+          f"pos=full_text&fromSearchLine=false&page={page}"
+
+    headers = {
+        'Accept': '*/*',
+        'User-Agent': useragent.random
+    }
+
+    with requests.Session() as session:
+        response = session.get(url=url, headers=headers)
+
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    soup.find(attrs={'key': 'value'})
+
+    print(response.status_code)
+
+    if not os.path.exists('data'):
+        os.mkdir('data')
+
+    # with open('data/hh_data.json', 'w', encoding='utf-8') as file:
+    #     json.dump(response.json(), file, indent=4, ensure_ascii=False)
+
+
+def main():
+    get_data()
+
+
+if __name__ == '__main__':
+    main()
