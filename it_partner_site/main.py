03a22160c5dfecf5221fbbfcfcb87603aa456f64
maxbaranov <maxexplorer@mail.ru>
2023-11-15 12:09:46 +0300

    it_partner_site: refactor

--- it_partner_site/main.py
+++ it_partner_site/main.py
@@ -1,25 +1,64 @@
+# import requests
+# import json
+#
+# # Аутентификация
+# auth_url = "https://b2b.i-t-p.pro/api/2"
+# auth_data = {
+#     "request": {
+#         "method": "login",
+#         "model": "auth",
+#         "module": "quickfox"
+#     },
+#     "data": {
+#         "login": "100553",
+#         "password": "BaUSEj"
+#     },
+# }
+#
+# auth_headers = {
+#     'Content-Type': 'application/json'
+# }
+#
+# response = requests.post(auth_url, data=json.dumps(auth_data), headers=auth_headers)
+# res_auth = response.json()
+#
+# print(res_auth)
+
+
 import requests
 import json
 
-# Аутентификация
-auth_url = "https://b2b.i-t-p.pro/api/2"
-auth_data = {
+# Замените эти значения на свои реальные логин и пароль
+client_login = "100553"
+client_password = "BaUSEj"
+
+# Замените на реальный домен вашего API
+api_url = "https://b2b.i-t-p.pro/api/2"
+
+# Составление JSON-RPC запроса
+json_rpc_request = {
+    "data": {
+        "login": client_login,
+        "password": client_password
+    },
     "request": {
         "method": "login",
         "model": "auth",
         "module": "quickfox"
-    },
-    "data": {
-        "login": "100553",
-        "password": " BaUSEj"
-    },
+    }
 }
 
-auth_headers = {
-    'Content-Type': 'application/json'
+# Преобразование запроса в формат JSON
+json_data = json.dumps(json_rpc_request)
+
+# Установка заголовков HTTP
+headers = {
+    "Content-Type": "application/json"
 }
 
-response = requests.post(auth_url, data=json.dumps(auth_data), headers=auth_headers)
-res_auth = response.json()
+# Отправка POST запроса
+response = requests.post(api_url, data=json_data, headers=headers)
 
-print(res_auth)
+# Печать ответа
+print(response.json())
+
