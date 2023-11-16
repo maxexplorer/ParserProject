ca9e38281a62568d44c49b38133832f1dc43a058
maxbaranov <maxexplorer@mail.ru>
2023-11-16 14:05:13 +0300

    dns_site: refactor

--- dns_site/main.py
+++ dns_site/main.py
@@ -2,13 +2,14 @@ import requests
 import os
 
 url = "https://www.dns-shop.ru"
+url_api = "https://restapi.dns-shop.ru/v1/get-menu-header?shortMenu=1"
 headers = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
                   ' (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14931'
 }
 
-def get_html(url, headers, session):
+def get_html(url: str, headers: dict, session: requests.sessions.Session) -> str:
     try:
         response = session.get(url=url, headers=headers, timeout=60)
         html = response.text
@@ -16,7 +17,17 @@ def get_html(url, headers, session):
     except Exception as ex:
         print(ex)
 
-def get_data(url, headers):
+def get_json(url: str, headers: dict, session: requests.sessions.Session) -> dict:
+    try:
+        response = session.get(url=url, headers=headers, timeout=60)
+        print(response.status_code)
+        json = response.json()
+        return json
+    except Exception as ex:
+        print(ex)
+
+
+def get_data(url: str, headers: dict) -> None:
     try:
         with requests.Session() as session:
             response = session.get(url=url, headers=headers, timeout=60)
@@ -27,9 +38,10 @@ def get_data(url, headers):
 
 def main():
     session = requests.Session()
-    html = get_html(url=url, headers=headers, session=session)
-    print(html)
+    # html = get_html(url=url, headers=headers, session=session)
     # get_data(url=url, headers=headers)
+    json = get_json(url=url_api, headers=headers, session=session)
+    print(json)
 
 if __name__ == '__main__':
     main()
