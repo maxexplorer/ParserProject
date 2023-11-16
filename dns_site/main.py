e4e21643bff651e3b640e4dfcbdabf320cc5ebce
maxbaranov <maxexplorer@mail.ru>
2023-11-16 13:56:13 +0300

    dns_site: refactor

--- dns_site/main.py
+++ dns_site/main.py
@@ -1,7 +1,7 @@
 import requests
 import os
 
-url = "url"
+url = "https://www.dns-shop.ru"
 headers = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
@@ -16,17 +16,20 @@ def get_html(url, headers, session):
     except Exception as ex:
         print(ex)
 
-def get_data():
+def get_data(url, headers):
     try:
         with requests.Session() as session:
             response = session.get(url=url, headers=headers, timeout=60)
+            print(response.status_code)
     except Exception as ex:
         print(ex)
 
 
 def main():
     session = requests.Session()
-    get_html(url=url, headers=headers, session=session)
+    html = get_html(url=url, headers=headers, session=session)
+    print(html)
+    # get_data(url=url, headers=headers)
 
 if __name__ == '__main__':
     main()
