1f804141345ac3b97fdf8f5e3653c87ac94c1dfb
maxbaranov <maxexplorer@mail.ru>
2023-11-20 14:26:00 +0300

    teledom46_site: refactor

--- teledom46_site/main.py
+++ teledom46_site/main.py
@@ -1,10 +1,11 @@
-import time
-
 import requests
 import os
-from bs4 import BeautifulSoup
-from datetime import datetime
+import json
 import csv
+from bs4 import BeautifulSoup
+import time
+from datetime import datetime
+
 
 url = "http://teledom46.ru/"
 # url = "https://pcshop33.ru/"
@@ -91,7 +92,7 @@ def get_urls(category_urls_list, headers):
         if not os.path.exists('data'):
             os.mkdir('data')
 
-        with open('data/product_urls_list.txt', 'w', encoding='utf-8') as file:
+        with open('data/product_url_list.txt', 'w', encoding='utf-8') as file:
             print(*product_urls_list, file=file, sep='\n')
 
 
@@ -102,7 +103,7 @@ def get_data(file_path, headers):
         count_urls = len(product_urls_list)
 
     result_list = []
-    image_urls_list = []
+    image_list = []
     with requests.Session() as session:
         for j, product_url in enumerate(product_urls_list[0:10], 1):
             try:
@@ -124,6 +125,23 @@ def get_data(file_path, headers):
                 title = None
 
             try:
+                image_data = soup.find_all('a', class_='product-detail-gallery__link popup_link fancy')
+
+                for item in image_data:
+                    try:
+                        img_url = "http://teledom46.ru" + item.get('href')
+                    except Exception:
+                        img_url = None
+                    image_list.append(
+                        {
+                            article_number: "http://teledom46.ru" + img_url
+                        }
+                    )
+            except Exception as ex:
+                print(ex)
+                continue
+
+            try:
                 price = soup.find('span', class_='price_value').text.strip()
             except Exception:
                 price = None
@@ -139,25 +157,67 @@ def get_data(file_path, headers):
             except Exception:
                 description = None
 
-            print(description)
 
             result_list.append(
                 (
                     article_number,
                     title,
                     price,
-                    characteristic
+                    characteristic,
+                    description
                 )
             )
 
+    return result_list, image_list
+
+def download_imgs(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        image_dict = json.load(file)
+
+    count_urls = len(image_dict)
+    count = 1
+
+    for art_num, img_url in image_dict.items():
+        response = requests.get(url=img_url)
+
+        if not os.path.exists(f"images/{art_num}"):
+            os.mkdir(f"data/{art_num}")
+
+        with open(f"images/{art_num}", "wb") as file:
+            file.write(response.content)
+
+        print(f'Обработано изображений: {count}/{count_urls}')
+
+        count += 1
+
+def save_json(data):
+    if not os.path.exists('data'):
+        os.mkdir('data')
+
+    with open('data/image_data.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+    print('Данные сохранены в файл "data.json"')
 
 def save_csv(data):
-    cur_time = datetime.now().strftime('%d-%m-%Y-%H-%M')
+    cur_date = datetime.now().strftime('%d-%m-%Y')
 
     if not os.path.exists('data'):
         os.mkdir('data')
 
-    with open(f'data/data_{cur_time}.csv', 'w', encoding='utf-8', newline='') as file:
+    with open(f'data/data_{cur_date}.csv', 'w', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            (
+                'Артикул',
+                'Название',
+                'Цена',
+                'Характеристики',
+                'Описание'
+            )
+        )
+
+    with open(f'data/data_{cur_date}.csv', 'w', encoding='utf-8', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(
             data
@@ -167,8 +227,10 @@ def save_csv(data):
 
 def main():
     # get_urls(category_urls_list=category_urls_list, headers=headers)
-    result_list = get_data(file_path="data/product_urls_list.txt", headers=headers)
-    # save_csv(data=result_list)
+    result_list, image_url_list = get_data(file_path="data/product_url_list.txt", headers=headers)
+    save_json(data=image_url_list)
+    save_csv(data=result_list)
+    download_imgs(file_path="data/image_data.json")
 
     execution_time = datetime.now() - start_time
     print('Сбор данных завершен!')
