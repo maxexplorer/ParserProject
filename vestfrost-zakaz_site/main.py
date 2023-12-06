94ee90fc3523cf0ceae80adf65a85bdf7aa99c37
maxbaranov <maxexplorer@mail.ru>
2023-12-06 09:13:37 +0300

    vestfrost_zakaz_site: add def get_pages()

--- vestfrost-zakaz_site/main.py
+++ vestfrost-zakaz_site/main.py
@@ -57,6 +57,64 @@ def get_pages(html: str) -> int:
     return pages
 
 
+# Получаем ссылки товаров
+def get_product_urls(file_path: str, headers: dict) -> None:
+    """
+    :param file_path: str
+    :param headers: dict
+    :return: None
+    """
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        category_urls_list = [line.strip() for line in file.readlines()]
+
+    count_urls = len(category_urls_list)
+    print(f'Всего: {count_urls} подкатегорий')
+
+    with requests.Session() as session:
+        for i, category_url in enumerate(category_urls_list, 1):
+            product_urls_list = []
+
+            name_category = category_url.split('/')[-2]
+            try:
+                html = get_html(url=category_url, headers=headers, session=session)
+            except Exception as ex:
+                print(f"{category_url} - {ex}")
+                continue
+            pages = get_pages(html=html)
+            print(f'В категории {name_category}: {pages} страниц')
+
+            for page in range(1, pages + 1):
+                product_url = f"{category_url}?PAGEN_1={page}"
+                try:
+                    html = get_html(url=product_url, headers=headers, session=session)
+                except Exception as ex:
+                    print(f"{url} - {ex}")
+                    continue
+                soup = BeautifulSoup(html, 'lxml')
+
+                try:
+                    data = soup.find_all('div', class_='prod-card prod-card--small')
+                    for item in data:
+                        try:
+                            product_url = f"https://kuppersberg.ru{item.find('a').get('href')}"
+                        except Exception as ex:
+                            print(ex)
+                            continue
+                        product_urls_list.append(product_url)
+                except Exception as ex:
+                    print(ex)
+
+                print(f'Обработано: {page}/{pages} страниц')
+
+            print(f'Обработано: {i}/{count_urls} категорий')
+
+            if not os.path.exists('data/products'):
+                os.makedirs(f'data/products')
+
+            with open(f'data/products/{name_category}.txt', 'w', encoding='utf-8') as file:
+                print(*product_urls_list, file=file, sep='\n')
+
 def main():
     session = requests.Session()
     # html = get_html(url=category_urls_list[0], headers=headers, session=session)
