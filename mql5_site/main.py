1099e974f13eef831d5b611a791a32314840befd
maxbaranov <maxexplorer@mail.ru>
2023-12-08 14:51:12 +0300

    mql5_site: add get_data()

--- mql5_site/main.py
+++ mql5_site/main.py
@@ -1,8 +1,11 @@
+import time
+
 import requests
 import os
 import csv
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
+from random import randint
 from datetime import datetime
 
 start_time = datetime.now()
@@ -54,9 +57,10 @@ def get_pages(html: str) -> int:
 
 
 # Получаем ссылки товаров
-def get_card_urls(headers: dict) -> None:
+def get_card_urls(headers: dict, meta_trader: str) -> None:
     """
     :param headers: dict
+    :param meta_trader: str
     :return: None
     """
 
@@ -64,7 +68,7 @@ def get_card_urls(headers: dict) -> None:
         card_urls_list = []
 
         try:
-            html = get_html(url="https://www.mql5.com/ru/signals/mt5/page1", headers=headers, session=session)
+            html = get_html(url=f"https://www.mql5.com/ru/signals/{meta_trader}/page1", headers=headers, session=session)
         except Exception as ex:
             pages = 30
             print(f'Не удалось получить HTML страницы для получения количества страниц. Будет использовано значение '
@@ -75,9 +79,11 @@ def get_card_urls(headers: dict) -> None:
         print(f'Всего: {pages} страниц')
 
         for page in range(1, pages + 1):
+            time.sleep(randint(1, 3))
+
             useragent = UserAgent()
 
-            page_card_url = f"https://www.mql5.com/ru/signals/mt5/page{page}"
+            page_card_url = f"https://www.mql5.com/ru/signals/{meta_trader}/page{page}"
 
             headers = {
                 'Accept': '*/*',
@@ -110,13 +116,99 @@ def get_card_urls(headers: dict) -> None:
         if not os.path.exists('data/cards'):
             os.makedirs(f'data/cards')
 
-        with open(f'data/cards/card_urls_list.txt', 'w', encoding='utf-8') as file:
+        with open(f'data/cards/card_urls_list_{meta_trader}.txt', 'w', encoding='utf-8') as file:
             print(*card_urls_list, file=file, sep='\n')
 
 
-def main():
+# Получаем данные о товарах
+def get_data(file_path: str, headers: dict) -> list:
+    """
+    :param file_path: str
+    :param headers: dict
+    :return: list
+    """
 
-    get_card_urls(headers=headers)
+    with open(file_path, 'r', encoding='utf-8') as file:
+        card_urls_list = [line.strip() for line in file.readlines()]
+
+    count = len(card_urls_list)
+
+    print(f'Всего {count} карточек')
+
+    result_list = []
+
+    # with requests.Session() as session:
+    #     for i, product_url in enumerate(card_urls_list[:1], 1):
+    #
+    #         if i % 10 == 0:
+    #             useragent = UserAgent()
+    #
+    #             headers = {
+    #                 'Accept': '*/*',
+    #                 'User-Agent': useragent.random
+    #             }
+    #
+    #         try:
+    #             html = get_html(url=product_url, headers=headers, session=session)
+    #         except Exception as ex:
+    #             print(f"{product_url} - {ex}")
+    #             continue
+    #
+    #         with open('data/index.html', 'w', encoding='utf-8') as file:
+    #             file.write(html)
+
+    with open('data/index.html', 'r', encoding='utf-8') as file:
+        html = file.read()
+
+
+
+    soup = BeautifulSoup(html, 'lxml')
+
+
+    try:
+
+
+
+    # print(f'Обработано карточек: {i}/{count}')
+
+
+def save_csv(data):
+    cur_date = datetime.now().strftime('%d-%m-%Y')
+
+    if not os.path.exists('data/results'):
+        os.makedirs('data/results')
+
+    with open(f'data/results/{cur_date}.csv', 'w', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(
+            ('folder: Категория',
+             'article: Артикул',
+             'name: Название',
+             'price: Цена',
+             'image: Иллюстрация',
+             'body: Описание',
+             'amount : Количество',
+             )
+        )
+
+    with open(f'data/results/{cur_date}.csv', 'a', encoding='utf-8', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerows(
+            data
+        )
+    print('Данные сохранены в файл "data.csv"')
+
+
+def main():
+    meta_trader = 'mt5'
+
+    get_card_urls(headers=headers, meta_trader=meta_trader)
+
+    get_data(file_path='data/cards/card_urls_list.txt', headers=headers)
+
+    execution_time = datetime.now() - start_time
+    print('Сбор данных завершен!')
+    print(f'Время работы программы: {execution_time}')
 
 
 if __name__ == '__main__':
