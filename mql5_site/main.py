16d2e67e14a68c8a396ce99ab5cbba30291b6056
maxbaranov <maxexplorer@mail.ru>
2023-12-08 13:27:11 +0300

    mql5_site: add get_html()

--- mql5_site/main.py
+++ mql5_site/main.py
@@ -6,6 +6,8 @@ from datetime import datetime
 
 start_time = datetime.now()
 
+
+url = "https://www.mql5.com/ru/signals"
 headers = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
@@ -25,7 +27,7 @@ def get_html(url: str, headers: dict, session: requests.sessions.Session) -> str
         response = session.get(url=url, headers=headers, timeout=60)
 
         if response.status_code != 200:
-            print(response.status_code)
+            print(f'{url}: {response.status_code}')
 
         html = response.text
         return html
@@ -34,7 +36,33 @@ def get_html(url: str, headers: dict, session: requests.sessions.Session) -> str
 
 
 def main():
-    pass
+
+    # session = requests.Session()
+
+    # html = get_html(url=url, headers=headers, session=session)
+
+    # with open('data/index.html', 'w', encoding='utf-8') as file:
+    #     file.write(html)
+
+    with open('data/index.html', 'r', encoding='utf-8') as file:
+        html = file.read()
+
+# Получаем количество страниц
+def get_pages(html: str) -> int:
+    """
+    :param html: str
+    :return: int
+    """
+
+    soup = BeautifulSoup(html, 'lxml')
+    try:
+        pages = int(
+            soup.find('ul', class_='pages-nav__list').find_all('li', class_='pages-nav__item')[-2].find_next().text)
+    except Exception as ex:
+        print(ex)
+        pages = 1
+
+    return pages
 
 if __name__ == '__main__':
     main()
