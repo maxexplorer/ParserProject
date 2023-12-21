1f9c752eecb9bbaac8bcc85543204c40ecbb51ae
maxbaranov <maxexplorer@mail.ru>
2023-12-21 17:19:46 +0300

    evelux_site: refactor

--- evelux_site/main.py
+++ evelux_site/main.py
@@ -14,6 +14,27 @@ headers = {
                   ' (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14931'
 }
 
+category_urls_list = [
+    "https://evelux.ru/catalog/aksessuary/additional-accessories/",
+    "https://evelux.ru/catalog/aksessuary/soedinitelnye-elementy/",
+    "https://evelux.ru/catalog/aksessuary/filtry/",
+    "https://evelux.ru/catalog/vstraivaemaya/dukhovye-shkafy/?FILTER=DSHGAS",
+    "https://evelux.ru/catalog/vstraivaemaya/dukhovye-shkafy/?FILTER=DSHELECTRO",
+    "https://evelux.ru/catalog/vstraivaemaya/varochnye-poverkhnosti/?FILTER=GAZ",
+    "https://evelux.ru/catalog/vstraivaemaya/varochnye-poverkhnosti/?FILTER=ELECTRO",
+    "https://evelux.ru/catalog/vstraivaemaya/varochnye-poverkhnosti/?FILTER=INDUCTION",
+    "https://evelux.ru/catalog/vstraivaemaya/vytyazhki/",
+    "https://evelux.ru/catalog/vstraivaemaya/posudomoechnye-mashiny/",
+    "https://evelux.ru/catalog/vstraivaemaya/mikrovolnovye-pechi/",
+    "https://evelux.ru/catalog/vstraivaemaya/stiralnye-mashiny/",
+    "https://evelux.ru/catalog/vstraivaemaya/kholodilniki/",
+    "https://evelux.ru/catalog/komplekty/",
+    "https://evelux.ru/catalog/otdelnostoyashaja/stiralnye-mashiny-solo/",
+    "https://evelux.ru/catalog/otdelnostoyashaja/sushilnye-mashiny/",
+    "https://evelux.ru/catalog/otdelnostoyashaja/kholodilniki-solo/",
+    ""
+]
+
 
 # Получаем html разметку страницы
 def get_html(url: str, headers: dict, session: requests.sessions.Session) -> str:
@@ -35,37 +56,62 @@ def get_html(url: str, headers: dict, session: requests.sessions.Session) -> str
     except Exception as ex:
         print(ex)
 
-# Получаем ссылки всех категорий товаров
-def get_category_urls(url: str, headers: dict) -> list:
+
+# Получаем ссылки товаров
+def get_product_urls(category_urls_list: list, headers: dict) -> None:
     """
-    :param url: str
+    :param file_path: list
     :param headers: dict
-    :return: list
+    :return: None
     """
 
-    category_urls_list = []
+    count_urls = len(category_urls_list)
+    print(f'Всего: {count_urls} категорий')
 
     with requests.Session() as session:
-        html = get_html(url=url, headers=headers, session=session)
+        for i, category_url in enumerate(category_urls_list, 1):
+            product_urls_list = []
 
-        soup = BeautifulSoup(html, 'lxml')
+            name_category = category_url.split('/')[-1]
 
-        try:
-            data = soup.find('div', class_='catalog-main__menu').find_all('a', class_='catalog-main__menu-item')
+            try:
+                html = get_html(url=category_url, headers=headers, session=session)
+            except Exception as ex:
+                print(f"{category_url} - {ex}")
+                continue
 
-            for item in data:
-                category_url = f"https://evelux.ru/catalog{item.get('href')}"
+            for page in range(1, 2):
+                page_product_url = f"{category_url}"
+                try:
+                    html = get_html(url=page_product_url, headers=headers, session=session)
+                except Exception as ex:
+                    print(f"{page_product_url} - {ex}")
+                    continue
+                soup = BeautifulSoup(html, 'lxml')
 
-                category_urls_list.append(category_url)
+                try:
+                    data = soup.find_all('div', class_='goods__item-slider owl-carousel owl-theme')
 
-        except Exception as ex:
-            print(ex)
+                    for item in data:
+                        try:
+                            product_url = f"{'/'.join(page_product_url.split('/')[:-1])}/{item.find('a').get('href')}"
+                            print(product_url)
+                        except Exception as ex:
+                            print(ex)
+                            continue
+                        # product_urls_list.append(product_url)
+                except Exception as ex:
+                    print(ex)
 
-        if not os.path.exists('data/categories'):
-            os.makedirs('data/categories')
+                # print(f'Обработано: {page}/{pages} страниц')
 
-        with open(f'data/categories/category_urls_list.txt', 'w', encoding='utf-8') as file:
-            print(*data, file=file, sep='\n')
+            # print(f'Обработано: {i}/{count_urls} категорий')
+
+            # if not os.path.exists('data/products'):
+            #     os.makedirs(f'data/products')
+            #
+            # with open(f'data/products/{name_category}.txt', 'w', encoding='utf-8') as file:
+            #     print(*product_urls_list, file=file, sep='\n')
 
 
 def save_csv(name, data):
@@ -97,7 +143,9 @@ def save_csv(name, data):
 
 
 def main():
-    get_category_urls(url=url, headers=headers)
+    category_urls_list = ["https://evelux.ru/catalog/vstraivaemaya/dukhovye-shkafy/?FILTER=DSHELECTRO"]
+
+    get_product_urls(category_urls_list=category_urls_list, headers=headers)
 
     execution_time = datetime.now() - start_time
     print('Сбор данных завершен!')
