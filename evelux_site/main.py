af8188a9a03d613274f316a327c7a0b91d3003d0
maxbaranov <maxexplorer@mail.ru>
2023-12-21 12:38:49 +0300

    evelux_site: add def save_scv()

--- evelux_site/main.py
+++ evelux_site/main.py
@@ -6,15 +6,14 @@ from datetime import datetime
 
 start_time = datetime.now()
 
-
 url = "https://evelux.ru/catalog/"
-
 
 headers = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
                   ' (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14931'
 }
+
 
 # Получаем html разметку страницы
 def get_html(url: str, headers: dict, session: requests.sessions.Session) -> str:
@@ -37,6 +36,34 @@ def get_html(url: str, headers: dict, session: requests.sessions.Session) -> str
         print(ex)
 
 
+def save_csv(name, data):
+    cur_date = datetime.now().strftime('%d-%m-%Y')
+
+    if not os.path.exists('data/results'):
+        os.makedirs('data/results')
+
+    with open(f'data/results/{name}_{cur_date}.csv', 'w', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(
+            ('vendor: Производитель',
+             'folder: Категория',
+             'article: Артикул',
+             'name: Название',
+             'price: Цена',
+             'image: Иллюстрация',
+             'body: Описание',
+             'amount : Количество'
+             )
+        )
+
+    with open(f'data/results/{name}_{cur_date}.csv', 'a', encoding='utf-8', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerows(
+            data
+        )
+    print('Данные сохранены в файл "data.csv"')
+
+
 def main():
     session = requests.Session()
     html = get_html(url=url, headers=headers, session=session)
@@ -44,6 +71,7 @@ def main():
     execution_time = datetime.now() - start_time
     print('Сбор данных завершен!')
     print(f'Время работы программы: {execution_time}')
+
 
 if __name__ == '__main__':
     main()
