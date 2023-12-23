40ed38110567c80395a8634d0f1cd327fdb884e7
maxbaranov <maxexplorer@mail.ru>
2023-12-23 10:27:39 +0300

    teka_site: add def get_product_urls()

--- teka_site/main.py
+++ teka_site/main.py
@@ -6,6 +6,9 @@ from datetime import datetime
 
 start_time = datetime.now()
 
+
+url = "https://www.teka.com/ru-ru/kuhni/duhovye-shkafy/"
+
 headers = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
@@ -34,7 +37,60 @@ def get_html(url: str, headers: dict, session: requests.sessions.Session) -> str
         print(ex)
 
 
+
+# Получаем ссылки товаров
+def get_product_urls(category_urls_list: list, headers: dict) -> None:
+    """
+    :param file_path: list
+    :param headers: dict
+    :return: None
+    """
+
+    count_urls = len(category_urls_list)
+    print(f'Всего: {count_urls} категорий')
+
+    with requests.Session() as session:
+        for i, category_url in enumerate(category_urls_list, 1):
+            product_urls_list = []
+
+            name_category = category_url.split('/')[-1]
+
+            try:
+                html = get_html(url=category_url, headers=headers, session=session)
+            except Exception as ex:
+                print(f"{category_url} - {ex}")
+                continue
+
+            soup = BeautifulSoup(html, 'lxml')
+
+            try:
+                data = soup.find_all('div', class_='catalog-card__text-content')
+                for item in data:
+                    try:
+                        product_url = f"https://asko-russia.ru{item.find('a').get('href')}"
+                    except Exception as ex:
+                        print(ex)
+                        continue
+                    product_urls_list.append(product_url)
+            except Exception as ex:
+                print(ex)
+
+        print(f'Обработано: {i}/{count_urls} категорий')
+
+        if not os.path.exists('data/products'):
+            os.makedirs(f'data/products')
+
+        with open(f'data/products/{name_category}.txt', 'w', encoding='utf-8') as file:
+            print(*product_urls_list, file=file, sep='\n')
+
+
 def main():
+
+    session = requests.Session()
+
+    html = get_html(url=url, headers=headers, session=session)
+
+
 
 
     execution_time = datetime.now() - start_time
