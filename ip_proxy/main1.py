f87fb3f8a3fdcdcfe95daff3989b98416f434459
maxbaranov <maxexplorer@mail.ru>
2024-01-06 11:05:10 +0300

    ip_proxy/main1: add def get_ip()

--- ip_proxy/main1.py
+++ ip_proxy/main1.py
@@ -9,10 +9,16 @@ headers = {
                   ' (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14931'
 }
 
-def get_html(url: str, headers: dict) -> str:
+
+def get_html(url: str, headers: dict = None) -> str:
     try:
         session = requests.Session()
-        response = session.get(url=url, headers=headers)
+
+        session.proxies = {
+            'http': 'http://103.48.69.54:82',
+        }
+
+        response = session.get(url=url, headers=headers, timeout=3)
 
         if response.status_code != 200:
             print(response.status_code)
@@ -22,7 +28,7 @@ def get_html(url: str, headers: dict) -> str:
         print(ex)
 
 
-def get_ip(url: str, headers: dict) -> str:
+def get_ip(url: str, headers: dict = None) -> str:
     html = get_html(url=url, headers=headers)
 
     soup = BeautifulSoup(html, 'lxml')
@@ -31,12 +37,14 @@ def get_ip(url: str, headers: dict) -> str:
         ip = soup.find('span', class_="ip").text.strip()
         ua = soup.find('span', class_="ip").find_next_sibling('span').text.strip()
 
+        print(ip, ua)
+
     except Exception as ex:
         print(ex)
 
 
 def main():
-    pass
+    get_ip(url=url, headers=headers)
 
 
 if __name__ == '__main__':
