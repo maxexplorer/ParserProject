4ae18399b682f057c98898632edcd68476567d29
maxbaranov <maxexplorer@mail.ru>
2024-01-06 10:41:00 +0300

    ip_proxy/main1: refactor

---
+++ ip_proxy/main1.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://sitespy.ru/my-ip"
+
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+                  ' (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14931'
+}
+
+def get_html(url: str, headers: dict) -> str:
+    try:
+        session = requests.Session()
+        response = session.get(url=url, headers=headers)
+
+        if response.status_code != 200:
+            print(response.status_code)
+        html = response.text
+        return html
+    except Exception as ex:
+        print(ex)
+
+
+def get_ip(url: str, headers: dict) -> str:
+    html = get_html(url=url, headers=headers)
+
+    soup = BeautifulSoup(html, 'lxml')
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
