2dace4e5f8f7b236dc3d2a970fe5829158c4a975
maxbaranov <maxexplorer@mail.ru>
2024-01-13 09:12:22 +0300

    selenium_cloudflare: refactor

--- selenium_cloudflare/selenium_cloudflare.py
+++ selenium_cloudflare/selenium_cloudflare.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from fake_useragent import UserAgent
 import time
 
 # options = webdriver.ChromeOptions()
 # is equivalent to
 options = Options()
-options.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                     ' Chrome/120.0.0.0 Safari/537.36')
+useragent = UserAgent()
+# options.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+#                      ' Chrome/120.0.0.0 Safari/537.36')
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
@@ -25,7 +27,8 @@ driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
 
 try:
     driver.maximize_window()
-    driver.get(url="https://www.sneakersnstuff.com/")
+    # driver.get(url="https://www.sneakersnstuff.com/")
+    driver.get(url="https://www.whatismybrowser.com/detect/what-is-my-user-agent/")
     time.sleep(30)
 except Exception as ex:
     print(ex)
