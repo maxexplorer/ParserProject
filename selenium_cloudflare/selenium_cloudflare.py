a4e96b8055bb5e04d73a11090cce741f659ac9d2
maxbaranov <maxexplorer@mail.ru>
2024-01-13 09:04:09 +0300

    selenium_cloudflare: refactor

--- selenium_cloudflare/selenium_cloudflare.py
+++ selenium_cloudflare/selenium_cloudflare.py
@@ -6,6 +6,8 @@ import time
 # options = webdriver.ChromeOptions()
 # is equivalent to
 options = Options()
+options.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                     ' Chrome/120.0.0.0 Safari/537.36')
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
