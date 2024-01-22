1026223ccd1f506efab2aa855695802aeb2fad56
maxbaranov <maxexplorer@mail.ru>
2024-01-22 14:08:19 +0300

    selenium: add driver_stealth.py

---
+++ selenium/driver_stealth.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium_stealth import stealth
+import time
+
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+
+# options.add_argument("--headless")
+
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+driver = webdriver.Chrome(options=options)
+
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+        )
+
+url = "https://bot.sannysoft.com/"
+driver.get(url)
+time.sleep(5)
+driver.quit()
