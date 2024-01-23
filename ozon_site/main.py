949780d614a6da8b37b7689a796f1f185856d045
maxbaranov <maxexplorer@mail.ru>
2024-01-23 09:51:51 +0300

    ozone_site: refactor

--- ozon_site/main.py
+++ ozon_site/main.py
@@ -13,18 +13,40 @@ from fake_useragent import UserAgent
 import time
 
 
-
 def undetected_chromdriver():
     driver = Chrome()
     driver.maximize_window()
 
     driver = Chrome()
     driver.maximize_window()
+    driver.implicitly_wait(15)
 
     try:
-        # driver.get(url="https://ozon.ru/t/601oMbY")
-        driver.get(url="https://www.ozon.ru/cart")
-        time.sleep(5)
+        driver.get(url="https://ozon.ru/t/601oMbY")
+
+        basket = WebDriverWait(driver, 15).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.ql1'))
+        )
+
+        basket.click()
+
+
+        WebDriverWait(driver, 15).until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'div.ql1'), 'В корзине')
+        )
+
+        in_basket = driver.find_element(By.CSS_SELECTOR, '#layoutPage > div.b0 > div.container.b4 > div.q5k.rk0 > div.q5k.rk1.kq9.k9q > div.q5k.rk1.kq9.qk9 > div.l0q.ql2 > div > div.ql1 > div > div > div.rj9 > div > div > div > div.j4r > button > div.b235-a')
+        in_basket.click()
+
+        time.sleep(10)
+
+        quantity = driver.find_element(By.CSS_SELECTOR, 'input[inputmode = numeric]').get_attribute('max')
+
+        print(quantity)
+
+
+
+
 
         if not os.path.exists('data'):
             os.makedirs('data')
@@ -41,6 +63,7 @@ def undetected_chromdriver():
 
 def main():
     undetected_chromdriver()
+
 
 if __name__ == '__main__':
     main()
