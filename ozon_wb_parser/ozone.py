899941a4e8db6eb20c5355684008b3d0332e3667
maxbaranov <maxexplorer@mail.ru>
2024-03-04 15:05:00 +0300

    ozone_wb_parser/ozone.py: add def get_data()

--- ozon_wb_parser/ozone.py
+++ ozon_wb_parser/ozone.py
@@ -2,6 +2,7 @@ import json
 import os
 import re
 import time
+from datetime import datetime
 from random import randint
 
 from undetected_chromedriver import Chrome
@@ -11,6 +12,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
+from pandas import DataFrame, ExcelWriter
+import openpyxl
+
+start_time = datetime.now()
 
 # Функция получения количества страниц
 def get_pages(html: str = None) -> int:
@@ -19,23 +24,19 @@ def get_pages(html: str = None) -> int:
 
         pages = int(soup.find('div', class_='eo6').find_all('a')[-2].text)
 
-        print(f'Всего: {pages} страниц!')
+        print(f'Всего страниц: {pages}')
     except Exception:
         pages = 1
 
     return pages
 
 
-# Функция полученя ссылок всех продуктов продавца
-def get_urls(file_path: str) -> list:
+# Функция получения ссылокб рейтинга, количества отзывов всех продуктов продавца
+def get_urls_rating_feedbacks(file_path: str) -> list[dict]:
     # Открываем файл в формате .txt
     with open(file_path, 'r', encoding='utf-8') as file:
         urls_list = [line.strip() for line in file.readlines()]
 
-    # Создаем объект опций
-    # options = ChromeOptions()
-    # Включение фонового режима
-    # options.add_argument('--headless')
     driver = Chrome()
     driver.maximize_window()
     driver.implicitly_wait(15)
@@ -43,7 +44,7 @@ def get_urls(file_path: str) -> list:
     product_data = []
 
     try:
-        for url in urls_list[:1]:
+        for url in urls_list:
 
             print(f'Обрабатывается: {url}')
 
@@ -79,13 +80,12 @@ def get_urls(file_path: str) -> list:
                         url_product = f"https://www.ozon.ru{item.find('a').get('href')}"
                     except Exception:
                         url_product = ''
-
                     try:
                         rating = item.find('div', class_='t8 t9 u tsBodyMBold').text.split()[0]
                     except Exception:
                         rating = ''
                     try:
-                        feedbacks_count =item.find('div', class_='t8 t9 u tsBodyMBold').text.split()[1]
+                        feedbacks_count = item.find('div', class_='t8 t9 u tsBodyMBold').text.split()[1]
                     except Exception:
                         feedbacks_count = ''
 
@@ -97,12 +97,6 @@ def get_urls(file_path: str) -> list:
                         }
                     )
 
-                if not os.path.exists('data'):
-                    os.makedirs('data')
-
-                with open('data/data.json', 'w', encoding='utf-8') as file:
-                    json.dump(product_data, file, indent=4, ensure_ascii=False)
-
         return product_data
 
     except Exception as ex:
@@ -111,84 +105,109 @@ def get_urls(file_path: str) -> list:
         driver.close()
         driver.quit()
 
+# Функция получения данных с карточки продукта
+def get_data(product_data: list) -> list[dict]:
 
-def get_data(file_path) -> list[dict]:
-    # # Открываем файл в формате .txt
-    # with open(file_path, 'r', encoding='utf-8') as file:
-    #     product_urls_list = [line.strip() for line in file.readlines()]
-    #
-    # # # Создаем объект опций
-    # # # options = ChromeOptions()
-    # # # Включение фонового режима
-    # # # options.add_argument('--headless')
-    # driver = Chrome()
-    # driver.maximize_window()
-    # driver.implicitly_wait(15)
-    # # result_list = []
-    # try:
-    #     for url in product_urls_list[:1]:
-    #         try:
-    #             driver.get(url=url)
-    #             time.sleep(randint(3, 5))
-    #             # driver.execute_script("window.scrollTo(0, 4000);")
-    #             # time.sleep(randint(3, 5))
-    #             # driver.execute_script("window.scrollTo(4000, 8000);")
-    #             # time.sleep(randint(8, 10))
-    #         except Exception as ex:
-    #             print(f'{url}: {ex}')
-    #             continue
-    #
-    #         with open('data/product.html', 'w', encoding='utf-8') as file:
-    #             file.write(driver.page_source)
-    #
-    #
-    #
-    # except Exception as ex:
-    #     print(ex)
-    # finally:
-    #     driver.close()
-    #     driver.quit()
+    driver = Chrome()
+    driver.maximize_window()
+    driver.implicitly_wait(15)
 
-    with open('data/product.html', 'r', encoding='utf-8') as file:
-        html = file.read()
+    result_list = []
 
-    soup = BeautifulSoup(html, 'lxml')
+    try:
+        for item in product_data[:10]:
+            try:
+                driver.get(url=item['url'])
+                time.sleep(randint(3, 5))
+                # driver.execute_script("window.scrollTo(0, 4000);")
+                # time.sleep(randint(3, 5))
+                # driver.execute_script("window.scrollTo(4000, 8000);")
+                # time.sleep(randint(8, 10))
+            except Exception as ex:
+                print(f"get_data: {item['url']} - {ex}")
+                continue
 
-    name = soup.find('div', {'data-widget': 'webProductHeading'}).find('h1').text.strip()
-    id_product = soup.find('div', {'data-widget': 'webDetailSKU'}).text.strip()
-    color = soup.find('span', string=re.compile('Цвет')).find_next().text.strip()
-    item_price = soup.find('div', {'data-widget': 'webPrice'})
+            html = driver.page_source
 
-    sale_price = ''.join(filter(lambda x: x.isdigit(), item_price.find('span', string=re.compile(
-        'без Ozon Карты')).find_previous().find_previous().find_previous().text))
 
-    ozone_price = ''.join(filter(lambda x: x.isdigit(), item_price.find('span', string=re.compile(
-        'c Ozon Картой')).find_parent().text))
+            soup = BeautifulSoup(html, 'lxml')
 
-    print(ozone_price)
+            try:
+                name = soup.find('div', {'data-widget': 'webProductHeading'}).find('h1').text.strip()
+            except Exception:
+                name = None
+            try:
+                brand = soup.find('a', class_='qj7').text.strip()
+            except Exception:
+                brand = None
+            try:
+                id_product = soup.find('div', {'data-widget': 'webDetailSKU'}).text.strip()
+            except Exception:
+                id_product = None
+            try:
+                color = soup.find('span', string=re.compile('Цвет')).find_next().text.strip()
+            except Exception:
+                color = None
 
-    # product_url = ''
-    #
-    # result_list.append(
-    #     {
-    #         'Wildberries': 'Wildberries',
-    #         'Ссылка': product_url,
-    #         'Бренд': brand,
-    #         'Название': name,
-    #         'SKU': id_product,
-    #         'Цвет': colors,
-    #         'РЦ+СПП': sale_price,
-    #         'Рейтинг': reviewRating,
-    #         'Кол-во отзывов': count_feedbacks,
-    #         'Последние 5 отзывов': average_rating,
-    #         'Цена с WB кошельком': wb_price
-    #     }
-    # )
+            try:
+                item_price = soup.find('div', {'data-widget': 'webPrice'})
+            except Exception:
+                item_price = None
+            try:
+                sale_price = ''.join(filter(lambda x: x.isdigit(), item_price.find('span', string=re.compile(
+                    'без Ozon Карты')).find_previous().find_previous().find_previous().text))
+            except Exception:
+                sale_price = None
+            try:
+                ozone_price = ''.join(filter(lambda x: x.isdigit(), item_price.find('span', string=re.compile(
+                    'c Ozon Картой')).find_parent().text))
+            except Exception:
+                ozone_price = None
 
+            result_list.append(
+                {
+                    'Ozone': 'Ozone',
+                    'Ссылка': item.get('url'),
+                    'Бренд': brand,
+                    'Название': name,
+                    'SKU': id_product,
+                    'Цвет': color,
+                    'РЦ+СПП': sale_price,
+                    'Рейтинг': item.get('rating'),
+                    'Кол-во отзывов': item.get('feedbacks_count'),
+                    'Цена с Ozone картой': ozone_price
+                }
+            )
+
+        return result_list
+
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.close()
+        driver.quit()
+
+# Функция для записи данных в формат xlsx
+def save_excel(data: list) -> None:
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
+    dataframe = DataFrame(data)
+
+    with ExcelWriter('data/result_list.xlsx', mode='w') as writer:
+        dataframe.to_excel(writer, sheet_name='Ozone', index=False)
+
+    print(f'Данные сохранены в файл "result_data.xlsx"')
 
 def main():
-    product_data = get_urls(file_path='data/urls_list_ozone.txt')
-    # ozone_data = get_data(file_path='data/product_urls_list.txt')
+    product_data = get_urls_rating_feedbacks(file_path='data/urls_list_ozone.txt')
+    ozone_data = get_data(product_data=product_data)
+    save_excel(data=ozone_data)
+
+    execution_time = datetime.now() - start_time
+    print('Сбор данных завершен!')
+    print(f'Время работы программы: {execution_time}')
+
 
 
 if __name__ == '__main__':
