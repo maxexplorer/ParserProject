a7c8537fff71e583256789cc54e604b3cad183a8
maxbaranov <maxexplorer@mail.ru>
2024-04-18 16:11:51 +0300

    zara: refactor

--- zara_site/functions.py
+++ zara_site/functions.py
@@ -4,9 +4,7 @@ from googletrans import Translator
 
 
 # Функция для перевода формата цветов Pull and Bear в Ozone
-def colors_format(value: str) -> str:
-
-
+def colors_format_ru(value: str) -> str:
     if value == 'АНТРАЦИТОВО-СЕРЫЙ':
         color = 'серый'
     elif value == 'ЖЕЛТОВАТО-БЕЛЫЙ':
@@ -439,6 +437,322 @@ def colors_format(value: str) -> str:
         color = 'зеленый'
     else:
         color = value.lower()
+
+    return color
+
+
+# Функция для перевода формата цветов Pull and Bear в Ozone
+def colors_format_en(value: str) -> str:
+    if value == 'WHITE':
+        color = 'белый'
+    elif value == 'KHAKI':
+        color = 'хаки'
+    elif value == 'BLACK':
+        color = 'черный'
+    elif value == 'BROWN_TAUPE':
+        color = 'серый'
+    elif value == 'NAVYBLUE':
+        color = 'темно-синий'
+    elif value == 'BLUE':
+        color = 'голубой'
+    elif value == 'BRIGHTRED':
+        color = 'красный'
+    elif value == 'BLUE_WHITE':
+        color = 'синий'
+    elif value == 'PEARLGREY':
+        color = 'светло-серый'
+    elif value == 'ANTHRACITEGREY':
+        color = 'светло-серый'
+    elif value == 'ECRU':
+        color = 'бежевый'
+    elif value == 'FUCHSIA':
+        color = 'перламутровый'
+    elif value == 'GREYMARL':
+        color = 'серый'
+    elif value == 'BLUE':
+        color = 'синий'
+    elif value == 'STONE':
+        color = 'серый'
+    elif value == 'MID-GREY':
+        color = 'серый'
+    elif value == 'ECRU':
+        color = 'светло-бежевый'
+    elif value == 'GREY':
+        color = 'серый'
+    elif value == 'LIGHTBROWN':
+        color = 'коричневый'
+    elif value == 'LIGHTKHAKI':
+        color = 'хаки'
+    elif value == 'SAND':
+        color = 'коричневый'
+    elif value == 'LIGHTBLUE':
+        color = 'синий'
+    elif value == 'BLUEGREY':
+        color = 'серый'
+    elif value == 'ICE':
+        color = 'светло-синий'
+    elif value == 'BLACK_SILVER':
+        color = 'серебристый'
+    elif value == 'LIGHTBEIGE':
+        color = 'светло-бежевый'
+    elif value == 'BEIGE-PINK':
+        color = 'розовый'
+    elif value == 'CAMEL':
+        color = 'желтый'
+    elif value == 'CAMEL':
+        color = 'светло-бежевый'
+    elif value == 'MID-BLUE':
+        color = 'светло-синий'
+    elif value == 'GREY_BEIGE':
+        color = 'серый'
+    elif value == 'DARKRED':
+        color = 'красный'
+    elif value == 'RED':
+        color = 'красный'
+    elif value == 'SAND':
+        color = 'светло-бежевый'
+    elif value == 'MULTICOLOURED':
+        color = 'разноцветный'
+    elif value == 'ECRU_BLACK':
+        color = 'черный'
+    elif value == 'CHOCOLATE':
+        color = 'коричневый'
+    elif value == 'BEIGE':
+        color = 'светло-бежевый'
+    elif value == 'CAMEL_WHITE':
+        color = 'белый'
+    elif value == 'ECRU_BLUE':
+        color = 'голубой'
+    elif value == 'BLUES':
+        color = 'синий'
+    elif value == 'BEIGEMARL':
+        color = 'бежевый'
+    elif value == 'LIGHTECRU':
+        color = 'светло-бежевый'
+    elif value == 'BLUE_NAVY':
+        color = 'синий'
+    elif value == 'DARKANTHRACITE':
+        color = 'светло-серый'
+    elif value == 'DARKBROWN':
+        color = 'коричневый'
+    elif value == 'SILVER':
+        color = 'серебристый'
+    elif value == 'GREENISH':
+        color = 'зеленый'
+    elif value == 'MIDKHAKI':
+        color = 'хаки'
+    elif value == 'SKYBLUE':
+        color = 'голубой'
+    elif value == 'MOSSGREEN':
+        color = 'зеленый'
+    elif value == 'DARKKHAKI':
+        color = 'хаки'
+    elif value == 'BRONZE':
+        color = 'светло-коричневый'
+    elif value == 'MINK':
+        color = 'белый'
+    elif value == 'GREEN':
+        color = 'светло-синий'
+    elif value == 'GREEN':
+        color = 'зеленый'
+    elif value == 'DARKCAMEL':
+        color = 'желтый'
+    elif value == 'DARKGREY':
+        color = 'серый'
+    elif value == 'BLUISH':
+        color = 'серый'
+    elif value == 'LILAC_WHITE':
+        color = 'белый'
+    elif value == 'TANMARL':
+        color = 'белый'
+    elif value == 'SAND_BROWN':
+        color = 'коричневый'
+    elif value == 'CHARCOALGREY':
+        color = 'серый'
+    elif value == 'TOBACCO':
+        color = 'коричневый'
+    elif value == 'BLACK_ECRU':
+        color = 'бежевый'
+    elif value == 'WINE':
+        color = 'бордовый'
+    elif value == 'LIGHTCAMEL':
+        color = 'желтый'
+    elif value == 'BLACKGOLD':
+        color = 'золотой'
+    elif value == 'BROWN':
+        color = 'коричневый'
+    elif value == 'ECRU_MAROON':
+        color = 'бежевый'
+    elif value == 'DARKGREEN':
+        color = 'зеленый'
+    elif value == 'CREAM':
+        color = 'бежевый'
+    elif value == 'MINK':
+        color = 'бежевый'
+    elif value == 'LIGHTGREY':
+        color = 'светло-серый'
+    elif value == 'STRIPED':
+        color = 'белый'
+    elif value == 'CAVA':
+        color = 'белый'
+    elif value == 'TAUPEGREY':
+        color = 'серый'
+    elif value == 'BEIGE':
+        color = 'бежевый'
+    elif value == 'NAVY_WHITE':
+        color = 'белый'
+    elif value == 'BLACK_GREEN':
+        color = 'зеленый'
+    elif value == 'GOLDEN':
+        color = 'золотой'
+    elif value == 'PETROLBLUE':
+        color = 'голубой'
+    elif value == 'OLIVEGREEN':
+        color = 'оливковый'
+    elif value == 'INKBLUE':
+        color = 'синий'
+    elif value == 'PINK_LILAC':
+        color = 'розовый'
+    elif value == 'EMERALD':
+        color = 'зеленый'
+    elif value == 'DARKBEIGE':
+        color = 'бежевый'
+    elif value == 'DARKGREYMARL':
+        color = 'серый'
+    elif value == 'BLACK_WHITE':
+        color = 'белый'
+    elif value == 'CHOCOLATEBROWN':
+        color = 'коричневый'
+    elif value == 'BLUE_INDIGO':
+        color = 'синий'
+    elif value == 'Charcoal':
+        color = 'серый'
+    elif value == 'BLUE_GREY':
+        color = 'серый'
+    elif value == 'INDIGO':
+        color = 'синий'
+    elif value == 'WASHEDPETROL':
+        color = 'белый'
+    elif value == 'SAND_MARL':
+        color = 'бежевый'
+    elif value == 'GOLD':
+        color = 'золотой'
+    elif value == 'INDIGO':
+        color = 'светло-синий'
+    elif value == 'BOTTLEGREEN':
+        color = 'разноцветный'
+    elif value == 'CHARCOAL':
+        color = 'белый'
+    elif value == 'GREY_NATURAL':
+        color = 'серый'
+    elif value == 'STRIPES':
+        color = 'белый'
+    elif value == 'LIGHTGREEN':
+        color = 'светло-зеленый'
+    elif value == 'DIRTY-MAUVE':
+        color = 'разноцветный'
+    elif value == 'ECRU_BEIGE':
+        color = 'бежевый'
+    elif value == 'MIDNIGHTBLUE':
+        color = 'голубой'
+    elif value == 'BLUISH':
+        color = 'синий'
+    elif value == 'APPLEGREEN':
+        color = 'applegreen'
+    elif value == 'PINK_WHITE':
+        color = 'pink_white'
+    elif value == 'Red/White':
+        color = 'red/white'
+    elif value == 'Светло-бежевый':
+        color = 'светло-бежевый'
+    elif value == 'PINK':
+        color = 'розовый'
+    elif value == 'MID-CAMEL':
+        color = 'желтый'
+    elif value == 'MID-PINK':
+        color = 'розовый'
+    elif value == 'INTENSERED':
+        color = 'красный'
+    elif value == 'GREY_BLUE':
+        color = 'серый'
+    elif value == 'LIGHTYELLOW':
+        color = 'желтый'
+    elif value == 'MID-MINK':
+        color = 'mid-mink'
+    elif value == 'BONE':
+        color = 'белый'
+    elif value == 'BRICK':
+        color = 'белый'
+    elif value == 'MID-GREEN':
+        color = 'зеленый'
+    elif value == 'DUSTYPINK':
+        color = 'розовый'
+    elif value == 'GREEN_ECRU':
+        color = 'бежевый'
+    elif value == 'MAROON':
+        color = 'бордовый'
+    elif value == 'DUCKGREEN':
+        color = 'зеленый'
+    elif value == 'MID-TURQUOISE':
+        color = 'голубой'
+    elif value == 'GREEN_BLUE':
+        color = 'голубой'
+    elif value == 'DARKMAUVE':
+        color = 'белый'
+    elif value == 'FADEDPINK':
+        color = 'розовый'
+    elif value == 'MAROONGREY':
+        color = 'серый'
+    elif value == 'REDDISH':
+        color = 'красный'
+    elif value == 'ORANGE':
+        color = 'оранжевый'
+    elif value == 'MID-ECRU':
+        color = 'бежевый'
+    elif value == 'PALEPINK':
+        color = 'розовый'
+    elif value == 'SEAGREEN':
+        color = 'светло-синий'
+    elif value == 'BLUE_GREEN':
+        color = 'зеленый'
+    elif value == 'ECRU_MARL':
+        color = 'бежевый'
+    elif value == 'PRINTED':
+        color = 'белый'
+    elif value == 'APRICOT':
+        color = 'оранжевый'
+    elif value == 'MAUVE':
+        color = 'сиреневый'
+    elif value == 'MINKMARL':
+        color = 'белый'
+    elif value == 'TERRACOTTA':
+        color = 'белый'
+    elif value == 'GREYISH':
+        color = 'серый'
+    elif value == 'DARKNAVY':
+        color = 'синий'
+    elif value == 'OCHRE':
+        color = 'оранжевый'
+    elif value == 'ECRU_GREEN':
+        color = 'зеленый'
+    elif value == 'OCHRE':
+        color = 'коричневый'
+    elif value == 'ONLYONE':
+        color = 'розовый'
+    elif value == 'GREENMARL':
+        color = 'разноцветный'
+    elif value == 'TAUPE':
+        color = 'белый'
+    elif value == 'PINKMARL':
+        color = 'розовый'
+    elif value == 'LIGHTGREYMARL':
+        color = 'разноцветный'
+    elif value == 'STRAWBERRY':
+        color = 'разноцветный'
+    elif value == 'RASPBERRY':
+        color = 'малиновый'
+    else:
+        color = value
 
     return color
 
--- zara_site/main.py
+++ zara_site/main.py
@@ -11,25 +11,28 @@ from pandas import read_excel
 from configs.config import headers
 from configs.config import params
 from data.data import id_categories_list_rus
+from data.data import id_categories_list_baby
 
 from data.data import id_region_dict
 # from data.data import products_data_list
 
-from functions import colors_format
+from functions import colors_format_ru
+from functions import colors_format_en
 from functions import sizes_format
+from functions import translator
 from functions import get_exchange_rate
 from functions import chunks
 
 start_time = datetime.now()
 
-# base_currency = 'EUR'
-base_currency = 'KZT'
+base_currency = 'EUR'
+# base_currency = 'KZT'
 target_currency = 'RUB'
 
 rub = get_exchange_rate(base_currency=base_currency, target_currency=target_currency)
 
-# print(f'Курс EUR/RUB: {rub}')
-print(f'Курс KZT/RUB: {rub}')
+print(f'Курс EUR/RUB: {rub}')
+# print(f'Курс KZT/RUB: {rub}')
 
 result_data = []
 
@@ -196,8 +199,12 @@ def get_products_array(products_data_list: list, headers: dict, id_region: str) 
 
                     json_data = response.json()
 
-
-                    result_data = get_products_data(products_data=json_data, main_category=main_category, type_product=type_product)
+                    if id_region == 'kz/ru':
+                        result_data = get_products_data_ru(products_data=json_data, main_category=main_category,
+                                                           type_product=type_product)
+                    else:
+                        result_data = get_products_data_en(products_data=json_data, main_category=main_category,
+                                                           type_product=type_product)
 
                     count += len(chunk_ids)
 
@@ -213,7 +220,7 @@ def get_products_array(products_data_list: list, headers: dict, id_region: str) 
 
 
 # Функция получения данных товаров
-def get_products_data(products_data: dict, main_category: str, type_product: str) -> list:
+def get_products_data_ru(products_data: dict, main_category: str, type_product: str) -> list:
 
     for item in products_data:
         try:
@@ -242,7 +249,7 @@ def get_products_data(products_data: dict, main_category: str, type_product: str
 
         try:
             color_original = item['detail']['colors'][0]['name']
-            color_ru = colors_format(value=color_original)
+            color_ru = colors_format_ru(value=color_original)
         except Exception:
             color_original = None
             color_ru = None
@@ -369,7 +376,11 @@ def get_products_data(products_data: dict, main_category: str, type_product: str
                     id_product_size = None
 
                 if main_category == 'Девочки' or main_category == 'Мальчики':
-                    size_rus = ''.join(i for i in size_eur.split()[-2] if i.isdigit())
+                    try:
+                        size_rus = ''.join(i for i in size_eur.split()[-2] if i.isdigit())
+                    except Exception:
+                        size_rus = size_eur
+
                     if not size_rus:
                         size_rus = size_eur
                 else:
@@ -464,18 +475,279 @@ def get_products_data(products_data: dict, main_category: str, type_product: str
     return result_data
 
 
+# Функция получения данных товаров
+def get_products_data_en(products_data: dict, main_category: str, type_product: str) -> list:
+    for item in products_data:
+        try:
+            id_product = item['detail']['colors'][0]['productId']
+        except Exception:
+            id_product = None
+
+        try:
+            reference = item['detail']['reference'].split('-')[0]
+        except Exception:
+            reference = None
+
+        try:
+            name_product = item['name']
+            name_product = f'ZARA {translator(name_product)}'
+        except Exception:
+            name_product = None
+
+        if not name_product:
+            continue
+
+        try:
+            price = int(item['detail']['colors'][0]['price']) / 100
+            price = round(price * rub)
+        except Exception:
+            price = 0
+
+        try:
+            color_original = item['detail']['colors'][0]['name']
+            color_ru = colors_format_en(value=color_original)
+        except Exception:
+            color_original = None
+            color_ru = None
+
+        try:
+            id_color = item['detail']['colors'][0]['id']
+        except Exception:
+            id_color = ''
+
+        try:
+            image_urls_list = []
+            xmedia_items = item['detail']['colors'][0]['xmedia']
+            for xmedia_item in xmedia_items:
+                path_image = xmedia_item['path']
+                name_image = xmedia_item['name']
+                timestamp_image = xmedia_item['timestamp']
+
+                try:
+                    img_url = xmedia_item['url'].split('?')[0]
+                except Exception:
+                    img_url = f"https://static.zara.net/photos//{path_image}/w/750/{name_image}.jpg?ts={timestamp_image}"
+
+                image_urls_list.append(img_url)
+
+            main_image = image_urls_list[0]
+
+            additional_images = '; '.join(image_urls_list[1:])
+
+        except Exception:
+            main_image = None
+            additional_images = None
+
+
+        try:
+            if main_category == 'Женщины':
+                gender = 'женский'
+            elif main_category == 'Мужчины':
+                gender = 'мужской'
+            else:
+                gender = main_category
+        except Exception:
+            gender = None
+
+        try:
+            raw_description = ' '.join(item['detail']['colors'][0]['rawDescription'].split())
+            raw_description = translator(raw_description)
+            description = f"🚚 ДОСТАВКА ИЗ ЕВРОПЫ 🌍✈️<br/>✅ Регулярное обновление коллекций.<br/>✅ Полный ассортимент брендa Zara. Более 10 000 товаров ждут вас в профиле нашего магазина! 🏷️<br/>✅ Более простой поиск нужных вещей внутри нашего магазина. Подписывайтесь, чтобы всегда быть в курсе последних поступлений и акций! 🔍📲<br/>{raw_description}<br/>📣 При выборе товара ориентируйтесь на ЕВРОПЕЙСКИЙ размер!"
+        except Exception:
+            description = None
+
+        brand = 'Zara'
+
+        care = "Машинная стирка при температуре до 30ºC с коротким циклом отжима. Отбеливание запрещено. " \
+               "Гладить при температуре до 110ºC. Не использовать машинную сушку. Стирать отдельно."
+
+        if main_category == 'Женщины':
+            model_height = '175'
+        elif main_category == 'Мужчины':
+            model_height = '180'
+        else:
+            model_height = None
+
+        if main_category == 'Женщины':
+            model_size = '44'
+        elif main_category == 'Мужчины':
+            model_size = '48'
+        else:
+            model_size = None
+
+        try:
+            composition_items = item['detail']['detailedComposition']['parts']
+
+            material_outer_shell = None
+            material_lining = None
+            composition_outer_shell = None
+            composition_lining = None
+
+            for composition_item in composition_items:
+                composition = composition_item['description']
+                components_items = composition_item['components']
+                if composition == 'OUTER SHELL':
+                    try:
+                        material_outer_shell = components_items[0]['material']
+                        material_outer_shell = translator(material_outer_shell)
+                    except Exception:
+                        material_outer_shell = None
+
+                    try:
+                        composition_outer_shell = ' '.join(
+                            f"{components_item['material']}: {components_item['percentage']}" for components_item in
+                            components_items)
+                        composition_outer_shell = translator(composition_outer_shell)
+                    except Exception:
+                        composition_outer_shell = None
+                elif composition == 'LINING':
+                    try:
+                        material_lining = components_items[0]['material']
+                        material_lining = translator(material_lining)
+                    except Exception:
+                        material_lining = None
+                    try:
+                        composition_lining = ' '.join(
+                            f"{components_item['material']}: {components_item['percentage']}" for components_item in
+                            components_items)
+                        composition_lining = translator(composition_lining)
+                    except Exception:
+                        composition_lining = None
+
+            material = material_outer_shell if material_outer_shell else material_lining
+
+            composition_outer_shell = composition_outer_shell if composition_outer_shell else composition_lining
+
+        except Exception:
+            material = None
+            composition_outer_shell = None
+            composition_lining = None
+
+        try:
+            sizes_items = item['detail']['colors'][0]['sizes']
+
+            for size_item in sizes_items:
+                size_eur = size_item.get('name')
+                status_size = size_item.get('availability')
+
+                if color_original is not None:
+                    id_product_size = f"{id_product}/{color_original.replace(' ', '-')}/{size_eur}/{reference}"
+                else:
+                    id_product_size = None
+
+                if main_category == 'Девочки' or main_category == 'Мальчики' or main_category == 'Девочки;Мальчики':
+                    try:
+                        size_rus = ''.join(i for i in size_eur.split()[-2] if i.isdigit())
+                    except Exception:
+                        size_rus = size_eur
+
+                    if not size_rus:
+                        size_rus = size_eur
+                else:
+                    if size_eur.isdigit():
+                        size_rus = sizes_format(format='digit', gender=main_category, size_eur=size_eur)
+                    elif not size_eur.isdigit():
+                        size_rus = sizes_format(format='alpha', gender=main_category, size_eur=size_eur)
+                    else:
+                        size_rus = size_eur
+
+                result_data.append(
+                    {
+                        '№': None,
+                        'Артикул': id_product_size,
+                        'Название товара': name_product,
+                        'Цена, руб.*': price,
+                        'Цена до скидки, руб.': None,
+                        'НДС, %*': None,
+                        'Включить продвижение': None,
+                        'Ozon ID': id_product_size,
+                        'Штрихкод (Серийный номер / EAN)': None,
+                        'Вес в упаковке, г*': None,
+                        'Ширина упаковки, мм*': None,
+                        'Высота упаковки, мм*': None,
+                        'Длина упаковки, мм*': None,
+                        'Ссылка на главное фото*': main_image,
+                        'Ссылки на дополнительные фото': additional_images,
+                        'Ссылки на фото 360': None,
+                        'Артикул фото': None,
+                        'Бренд в одежде и обуви*': brand,
+                        'Объединить на одной карточке*': reference,
+                        'Цвет товара*': color_ru,
+                        'Российский размер*': size_rus,
+                        'Размер производителя': size_eur,
+                        'Статус наличия': status_size,
+                        'Название цвета': color_original,
+                        'Тип*': type_product,
+                        'Пол*': gender,
+                        'Размер пеленки': None,
+                        'ТН ВЭД коды ЕАЭС': None,
+                        'Ключевые слова': None,
+                        'Сезон': None,
+                        'Рост модели на фото': model_height,
+                        'Параметры модели на фото': None,
+                        'Размер товара на фото': model_size,
+                        'Коллекция': None,
+                        'Страна-изготовитель': None,
+                        'Вид принта': None,
+                        'Аннотация': description,
+                        'Инструкция по уходу': care,
+                        'Серия в одежде и обуви': None,
+                        'Материал': material,
+                        'Состав материала': composition_outer_shell,
+                        'Материал подклада/внутренней отделки': composition_lining,
+                        'Материал наполнителя': None,
+                        'Утеплитель, гр': None,
+                        'Диапазон температур, °С': None,
+                        'Стиль': None,
+                        'Вид спорта': None,
+                        'Вид одежды': None,
+                        'Тип застежки': None,
+                        'Длина рукава': None,
+                        'Талия': None,
+                        'Для беременных или новорожденных': None,
+                        'Тип упаковки одежды': None,
+                        'Количество в упаковке': None,
+                        'Состав комплекта': None,
+                        'Рост': None,
+                        'Длина изделия, см': None,
+                        'Длина подола': None,
+                        'Форма воротника/горловины': None,
+                        'Детали': None,
+                        'Таблица размеров JSON': None,
+                        'Rich-контент JSON': None,
+                        'Плотность, DEN': None,
+                        'Количество пар в упаковке': None,
+                        'Класс компрессии': None,
+                        'Персонаж': None,
+                        'Праздник': None,
+                        'Тематика карнавальных костюмов': None,
+                        'Признак 18+': None,
+                        'Назначение спецодежды': None,
+                        'HS-код': None,
+                        'Количество заводских упаковок': None,
+                        'Ошибка': None,
+                        'Предупреждение': None,
+                    }
+                )
+        except Exception as ex:
+            print(f'sizes: {ex}')
+
+    return result_data
+
+
+
 # Функция для записи данных в формат xlsx
 def save_excel(data: list) -> None:
     if not os.path.exists('results'):
         os.makedirs('results')
 
-    if not os.path.exists('results/result_data_zara.xlsx'):
+    if not os.path.exists('results/result_data_zara_baby.xlsx'):
         # Если файл не существует, создаем его с пустым DataFrame
-        with ExcelWriter('results/result_data_zara.xlsx', mode='w') as writer:
+        with ExcelWriter('results/result_data_zara_baby.xlsx', mode='w') as writer:
             DataFrame().to_excel(writer, sheet_name='ОЗОН', index=False)
 
     # Загружаем данные из файла
-    df = read_excel('results/result_data_zara.xlsx', sheet_name='ОЗОН')
+    df = read_excel('results/result_data_zara_baby.xlsx', sheet_name='ОЗОН')
 
     # Определение количества уже записанных строк
     num_existing_rows = len(df.index)
@@ -483,7 +755,7 @@ def save_excel(data: list) -> None:
     # Добавляем новые данные
     dataframe = DataFrame(data)
 
-    with ExcelWriter('results/result_data_zara.xlsx', mode='a', if_sheet_exists='overlay') as writer:
+    with ExcelWriter('results/result_data_zara_baby.xlsx', mode='a', if_sheet_exists='overlay') as writer:
         dataframe.to_excel(writer, startrow=num_existing_rows + 1, header=(num_existing_rows == 0), sheet_name='ОЗОН',
                            index=False)
 
@@ -491,10 +763,10 @@ def save_excel(data: list) -> None:
 
 
 def main():
-    region = 'Казахстан'
+    region = 'Германия'
     id_region = id_region_dict.get(region)
     # get_id_categories(headers=headers, params=params)
-    products_data_list = get_id_products(id_categories_list=id_categories_list_rus, headers=headers, params=params,
+    products_data_list = get_id_products(id_categories_list=id_categories_list_baby, headers=headers, params=params,
                                          id_region=id_region)
     get_products_array(products_data_list=products_data_list, headers=headers, id_region=id_region)
 
