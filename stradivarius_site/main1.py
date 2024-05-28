8460c2672640e26ce72824dbdbcbbf56926c7c65
maxbaranov <maxexplorer@mail.ru>
2024-05-28 16:15:38 +0300

    stradivarius_site: feat

--- stradivarius_site/main1.py
+++ stradivarius_site/main1.py
@@ -522,7 +522,7 @@ def main():
         value = input('Продолжить сбор новых товаров:\n1 - Да\n2 - Нет\n')
         if value == '1':
             get_products_array(products_data_list=products_new_data_list, headers=headers, id_region=id_region,
-                           species='products')
+                               species='products')
 
     execution_time = datetime.now() - start_time
     print('Сбор данных завершен!')
