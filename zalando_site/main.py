5ed4a08d6ad8134dcac6b3c628538be399fa4c2a
maxbaranov <maxexplorer@mail.ru>
2024-07-08 13:36:57 +0300

    zalando: feat

--- zalando_site/main.py
+++ zalando_site/main.py
@@ -506,21 +506,21 @@ def get_products_data(products_data_list: list[dict], brand: str) -> None:
 
                 print(f'Обработано: {i}/{count_products} товаров!')
 
-        save_excel(data=result_data, brand=brand)
+        save_excel(data=result_data, brand=brand, species='products')
 
 
 # Функция для записи данных в формат xlsx
-def save_excel(data: list, brand: str) -> None:
+def save_excel(data: list, brand: str, species: str) -> None:
     if not os.path.exists('results'):
         os.makedirs('results')
 
-    if not os.path.exists(f'results/result_data_{brand}.xlsx'):
+    if not os.path.exists(f'results/result_data_{species}_{brand}.xlsx'):
         # Если файл не существует, создаем его с пустым DataFrame
-        with ExcelWriter(f'results/result_data_{brand}.xlsx', mode='w') as writer:
+        with ExcelWriter(f'results/result_data_{species}_{brand}.xlsx', mode='w') as writer:
             DataFrame().to_excel(writer, sheet_name='ОЗОН', index=False)
 
     # Загружаем данные из файла
-    df = read_excel(f'results/result_data_{brand}.xlsx', sheet_name='ОЗОН')
+    df = read_excel(f'results/result_data_{species}_{brand}.xlsx', sheet_name='ОЗОН')
 
     # Определение количества уже записанных строк
     num_existing_rows = len(df.index)
@@ -528,7 +528,7 @@ def save_excel(data: list, brand: str) -> None:
     # Добавляем новые данные
     dataframe = DataFrame(data)
 
-    with ExcelWriter(f'results/result_data_{brand}.xlsx', mode='a',
+    with ExcelWriter(f'results/result_data_{species}_{brand}.xlsx', mode='a',
                      if_sheet_exists='overlay') as writer:
         dataframe.to_excel(writer, startrow=num_existing_rows + 1, header=(num_existing_rows == 0), sheet_name='ОЗОН',
                            index=False)
