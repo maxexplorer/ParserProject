311b885341d088c6c28b1df9ada52d03063d277c
maxbaranov <maxexplorer@mail.ru>
2024-07-08 13:34:06 +0300

    zalando: feat

--- zalando_site/main.py
+++ zalando_site/main.py
@@ -542,6 +542,8 @@ def main():
         'Введите значение:\n1 - Tommy Hilfiger\n2 - Jack & Jones\n3 - PEPE JEANS\n4 - CALVIN KLEIN\n5 - Scotch & Soda\n6 - GAP\n7 - Helly Hansen\n8 - The North Face\n9 - TOM TAILOR\n10 - s.Oliver\n11 - G-Star\n12 - Esprit\n13 - Guess\n14 - MANGO\n15 - Adidas\n16 - Nike\n17 - Puma\n18 - Vans\n19 - ASICS\n20 - Under Armour\n21 - Reebok\n22 - COLUMBIA')
     brand = brand_dict.get(value)
 
+    print(f'Сбор данных бренда: {brand}')
+
     if brand:
         driver = init_chromedriver(headless_mode=True)
         try:
