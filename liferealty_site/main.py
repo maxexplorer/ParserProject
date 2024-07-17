d84a90f1630625994358210d252690f11aa6a212
maxbaranov <maxexplorer@mail.ru>
2024-07-17 10:26:34 +0300

    liferealty_site: init commit

---
+++ liferealty_site/main.py
@@ -0,0 +1,258 @@
+import time
+from datetime import datetime
+import os
+import random
+from math import ceil
+
+from requests import Session
+from bs4 import BeautifulSoup
+
+from pandas import DataFrame, ExcelWriter, read_excel
+
+start_time = datetime.now()
+
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/126.0.0.0 Safari/537.36'
+}
+
+category_urls_list = [
+    'https://liferealty.ru/sale/',
+    'https://liferealty.ru/rent/',
+    'https://liferealty.ru/land/',
+    'https://liferealty.ru/compounds/'
+]
+
+
+# Получаем html разметку страницы
+def get_html(url: str, headers: dict, session: Session) -> str:
+    try:
+        response = session.get(url=url, headers=headers, timeout=60)
+
+        if response.status_code != 200:
+            print(f'status_code: {response.status_code}')
+
+        html = response.text
+        return html
+    except Exception as ex:
+        print(f'get_html: {ex}')
+
+
+# Получаем количество страниц
+def get_pages(html: str) -> int:
+    soup = BeautifulSoup(html, 'lxml')
+    try:
+        pages = ceil(int(soup.find('span', id='lots_count').text.strip()) / 32)
+    except Exception as ex:
+        print(f'get_pages: {ex}')
+        pages = 1
+
+    return pages
+
+
+# Получаем ссылки статей
+def get_article_urls(category_urls_list: list, headers: dict) -> None:
+    with Session() as session:
+        for i, category_url in enumerate(category_urls_list[:1], 1):
+            article_urls_list = []
+
+            try:
+                html = get_html(url=category_url, headers=headers, session=session)
+            except Exception as ex:
+                print(f"get_article_urls: {category_url} - {ex}")
+                continue
+
+            if not html:
+                print(f'not html: {category_url}')
+                continue
+
+            pages = get_pages(html=html)
+            print(f'В категории {category_url}: {pages} страниц!')
+
+            for page in range(1, pages + 1):
+                page_product_url = f"{category_url}{page}/"
+                try:
+                    time.sleep(random.randint(1, 3))
+                    html = get_html(url=page_product_url, headers=headers, session=session)
+                except Exception as ex:
+                    print(f"{page_product_url} - {ex}")
+                    continue
+
+                if not html:
+                    print(f'not html: {page_product_url}')
+                    continue
+
+                soup = BeautifulSoup(html, 'lxml')
+
+                try:
+                    category_title = soup.find('div', id='navdata').text.split('\t')[-1].strip()
+                except Exception:
+                    category_title = ''
+                try:
+                    data = soup.find_all('a', class_='alot_img')
+
+                    for item in data:
+                        try:
+                            product_url = f"https://liferealty.ru{item.get('href')}"
+                        except Exception as ex:
+                            print(f'product_url: {ex}')
+                            continue
+                        article_urls_list.append(product_url)
+                except Exception as ex:
+                    print(ex)
+
+                print(f'Обработано: {page}/{pages} страниц!')
+
+            if not os.path.exists('data'):
+                os.makedirs(f'data')
+
+            with open(f'data/{category_title}.txt', 'w', encoding='utf-8') as file:
+                print(*article_urls_list, file=file, sep='\n')
+
+
+# Получаем данные о статьях
+def get_data(file_path: str, headers: dict) -> list:
+    with open(file_path, 'r', encoding='utf-8') as file:
+        article_urls_list = [line.strip() for line in file.readlines()]
+
+    count = len(article_urls_list)
+
+    print(f'Всего {count} статей!')
+
+    result_data = []
+
+    with Session() as session:
+        for i, article_url in enumerate(article_urls_list, 1):
+            try:
+                time.sleep(random.randint(1, 3))
+                html = get_html(url=article_url, headers=headers, session=session)
+            except Exception as ex:
+                print(f"{article_url} - {ex}")
+                continue
+
+            if not html:
+                continue
+
+            soup = BeautifulSoup(html, 'lxml')
+
+            try:
+                data = soup.find('div', class_='central-content')
+            except Exception as ex:
+                print(f'data: {ex}')
+                continue
+            try:
+                article_title = data.find('h1').text.strip()
+            except Exception:
+                article_title = ''
+
+            try:
+                date = data.find('div', class_='date').text.strip()
+            except Exception:
+                date = ''
+
+            try:
+                statistic = data.find('div', class_='statistic').text.strip()
+            except Exception:
+                statistic = ''
+            try:
+                text = ' '.join(data.find('div', class_='f-text').text.split())
+            except Exception:
+                text = ''
+
+            result_data.append(
+                {
+                    'Ссылка': article_url,
+                    'Название статьи': article_title,
+                    'Дата издания': date,
+                    'Количество просмотров': statistic,
+                    'Текст': text,
+                }
+            )
+
+            print(f'Обработано товаров: {i}/{count}')
+
+    return result_data
+
+def download_imgs(file_path):
+    """Download images"""
+
+    try:
+        with open(file_path) as file:
+            src = json.load(file)
+    except Exception as _ex:
+        print(_ex)
+        return "[INFO] Check the file path!"
+
+    items_len = len(src)
+    count = 1
+
+    for item in src[:100]:
+        item_name = item.get("title")
+        item_imgs = item.get("images")
+
+        if not os.path.exists(f"data/{item_name}"):
+            os.mkdir(f"data/{item_name}")
+
+        for img in item_imgs:
+            r = requests.get(url=img["url"])
+
+            with open(f"data/{item_name}/{img['type']}.png", "wb") as file:
+                file.write(r.content)
+
+        print(f"[+] Download {count}/{items_len}")
+        count += 1
+
+    return "[INFO] Work finished!"
+
+
+
+# Функция для записи данных в формат xlsx
+def save_excel(data: list, species: str, brand: str) -> None:
+    if not os.path.exists('results'):
+        os.makedirs('results')
+
+    if not os.path.exists(f'results/result_data_{species}_{brand}.xlsx'):
+        # Если файл не существует, создаем его с пустым DataFrame
+        with ExcelWriter(f'results/result_data_{species}_{brand}.xlsx', mode='w') as writer:
+            DataFrame().to_excel(writer, sheet_name='ОЗОН', index=False)
+
+    # Загружаем данные из файла
+    df = read_excel(f'results/result_data_{species}_{brand}.xlsx', sheet_name='ОЗОН')
+
+    # Определение количества уже записанных строк
+    num_existing_rows = len(df.index)
+
+    # Добавляем новые данные
+    dataframe = DataFrame(data)
+
+    with ExcelWriter(f'results/result_data_{species}_{brand}.xlsx', mode='a',
+                     if_sheet_exists='overlay') as writer:
+        dataframe.to_excel(writer, startrow=num_existing_rows + 1, header=(num_existing_rows == 0), sheet_name='ОЗОН',
+                           index=False)
+
+    print(f'Данные сохранены в файл "result_data.xlsx"')
+
+
+
+
+
+def main():
+    get_article_urls(category_urls_list=category_urls_list, headers=headers)
+
+    # directory = 'data'
+    # for filename in os.listdir(directory):
+    #     file_path = os.path.join(directory, filename)
+    #     if os.path.isfile(file_path):
+    #         category_title = file_path.split('\\')[-1].split('.')[0]
+    #         print(f'Обрабатывается категория {category_title}')
+    #         result_list = get_data(file_path=file_path, headers=headers)
+    #         save_excel(data=result_list, category_title=category_title)
+
+    execution_time = datetime.now() - start_time
+    print('Сбор данных завершен!')
+    print(f'Время работы программы: {execution_time}')
+
+
+if __name__ == '__main__':
+    main()
