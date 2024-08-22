41a16f04fb7e7aabf6b7aad06b3d9d9be44cc35f
maxbaranov <maxexplorer@mail.ru>
2024-08-22 08:52:13 +0300

    ozon: refactor

--- ozon_wb_parser/main.py
+++ ozon_wb_parser/main.py
@@ -13,22 +13,22 @@ def main():
     value = input('Введите значение:\n1 - Ozone\n2 - Wildberries\n3 - Оба сайта\n')
 
     if value == '1':
-        print('Сбор данных Ozone')
+        print('Сбор данных Ozon')
         product_data = get_urls_rating_feedbacks(file_path='data/urls_list_ozone.txt')
         ozone_data = get_data_products_ozone(product_data=product_data)
         save_excel_ozone(data=ozone_data)
-        print('Сбор данных Ozone завершен')
+        print('Сбор данных Ozon завершен')
     elif value == '2':
         print('Сбор данных Wildberries')
         wb_data = get_data_products_wb(file_path='data/urls_list_wb.txt')
         save_excel_wb(data=wb_data)
         print('Сбор данных Wildberries завершен')
     elif value == '3':
-        print('Сбор данных Ozone')
+        print('Сбор данных Ozon')
         product_data = get_urls_rating_feedbacks(file_path='data/urls_list_ozone.txt')
         ozone_data = get_data_products_ozone(product_data=product_data)
         save_excel_ozone(data=ozone_data)
-        print('Сбор данных Ozone завершен')
+        print('Сбор данных Ozon завершен')
 
         print('Сбор данных Wildberries')
         wb_data = get_data_products_wb(file_path='data/urls_list_wb.txt')
