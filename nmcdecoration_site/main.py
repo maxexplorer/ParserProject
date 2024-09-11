3a32ed0232d01fa6d9cb99527ad74f5fc789579d
maxbaranov <maxexplorer@mail.ru>
2024-09-11 16:57:34 +0300

    nmcdecoration_site: refactor

--- nmcdecoration_site/main.py
+++ nmcdecoration_site/main.py
@@ -26,10 +26,6 @@ def get_data(data_list):
             try:
                 response = session.get(url=url, headers=headers, timeout=60)
 
-                if response.status_code != 200:
-                    print(f'url: {url} status_code: {response.status_code}')
-                    continue
-
                 soup = BeautifulSoup(response.text, 'lxml')
             except Exception:
                 exceptions_list.append(
