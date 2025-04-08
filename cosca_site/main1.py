3edf4f147143acb0331498c2e9246d860fa493fb
maxbaranov <maxexplorer@mail.ru>
2025-04-08 16:57:12 +0300

    refactor (cosca)

--- cosca_site/main1.py
+++ cosca_site/main1.py
@@ -26,7 +26,7 @@ category_urls_list = [
 ]
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36'
 }
 
 
