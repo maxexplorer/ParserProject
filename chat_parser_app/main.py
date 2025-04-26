8003fd9ab70412617962eef855e1f40632caa745
maxbaranov <maxexplorer@mail.ru>
2025-04-26 10:39:26 +0300

    modify (chat_parser_bot)

--- chat_parser_app/main.py
+++ chat_parser_app/main.py
@@ -5,4 +5,8 @@ from bot import start_bot  # Импортируем запуск бота
 
 if __name__ == "__main__":
 
-    start_bot()
+    try:
+        start_bot()
+    except Exception as ex:
+        print(f'main: {ex}')
+        input("Нажмите Enter, чтобы закрыть программу...")
