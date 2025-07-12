e83f5389619e222caecbf2f405c530e62aa2f632
maxbaranov <maxexplorer@mail.ru>
2025-07-12 18:51:38 +0300

    modify (ozon, wb)

--- ozon_wildberries_parser/run_main.py
+++ ozon_wildberries_parser/run_main.py
@@ -30,82 +30,88 @@ def main():
     except Exception:
         raise Exception('Введено неправильное значение')
 
-    match value:
-        case '1':
-            pages = int(input('Введите количество страниц Ozon: \n'))
-            driver = init_undetected_chromedriver()
-            try:
-                print('Сбор данных Ozon...')
-                ozon_parser(driver=driver, workbook=workbook, pages=pages)
-                print('Сбор данных Ozon завершен.')
-            except Exception as ex:
-                print(f'main: {ex}')
-                input('Нажмите Enter, чтобы закрыть программу...')
-            finally:
-                driver.close()
-                driver.quit()
+    try:
+        match value:
+            case '1':
+                pages = int(input('Введите количество страниц Ozon: \n'))
+                driver = init_undetected_chromedriver()
+                try:
+                    print('Сбор данных Ozon...')
+                    ozon_parser(driver=driver, workbook=workbook, pages=pages)
+                    print('Сбор данных Ozon завершен.')
+                except Exception as ex:
+                    print(f'main: {ex}')
+                finally:
+                    driver.close()
+                    driver.quit()
 
-        case '2':
-            pages = int(input('Введите количество страниц Wildberries: \n'))
-            try:
-                print('Сбор данных Wildberries...')
-                wildberries_parser(workbook=workbook, pages=pages)
-                print('Сбор данных Wildberries завершен.')
-            except Exception as ex:
-                print(f'main: {ex}')
-                input('Нажмите Enter, чтобы закрыть программу...')
+            case '2':
+                pages = int(input('Введите количество страниц Wildberries: \n'))
+                try:
+                    print('Сбор данных Wildberries...')
+                    wildberries_parser(workbook=workbook, pages=pages)
+                    print('Сбор данных Wildberries завершен.')
+                except Exception as ex:
+                    print(f'main: {ex}')
+                    input('Нажмите Enter, чтобы закрыть программу...')
 
-        case '3':
-            pages_ozon = int(input('Введите количество страниц Ozon: \n'))
-            pages_wb = int(input('Введите количество страниц Wildberries: \n'))
-            driver = init_undetected_chromedriver()
-            try:
-                print('Сбор данных Ozon...')
-                ozon_parser(driver=driver, workbook=workbook, pages=pages_ozon)
-                print('Сбор данных Ozon завершен.')
-            except Exception as ex:
-                print(f'main: {ex}')
-                input('Нажмите Enter, чтобы закрыть программу...')
-            finally:
-                driver.close()
-                driver.quit()
-            try:
-                print('Сбор данных Wildberries...')
-                wildberries_parser(workbook=workbook, pages=pages_wb)
-                print('Сбор данных Wildberries завершен.')
-            except Exception as ex:
-                print(f'main: {ex}')
-                input('Нажмите Enter, чтобы закрыть программу...')
+            case '3':
+                pages_ozon = int(input('Введите количество страниц Ozon: \n'))
+                pages_wb = int(input('Введите количество страниц Wildberries: \n'))
+                driver = init_undetected_chromedriver()
+                try:
+                    print('Сбор данных Ozon...')
+                    ozon_parser(driver=driver, workbook=workbook, pages=pages_ozon)
+                    print('Сбор данных Ozon завершен.')
+                except Exception as ex:
+                    print(f'main: {ex}')
+                    input('Нажмите Enter, чтобы закрыть программу...')
+                finally:
+                    driver.close()
+                    driver.quit()
+                try:
+                    print('Сбор данных Wildberries...')
+                    wildberries_parser(workbook=workbook, pages=pages_wb)
+                    print('Сбор данных Wildberries завершен.')
+                except Exception as ex:
+                    print(f'main: {ex}')
+                    input('Нажмите Enter, чтобы закрыть программу...')
 
-        case '4':
-            print('Обновление цен Ozon...')
-            article_info = load_article_info_from_excel(sheet_name='ОЗОН')
-            current_prices = update_prices_ozon(article_info=article_info)
-            write_price_to_excel(current_prices=current_prices, marketplace='ОЗОН')
-            print('✅ Цены Ozon обновлены и сохранены.')
+            case '4':
+                print('Обновление цен Ozon...')
+                article_info = load_article_info_from_excel(sheet_name='ОЗОН')
+                current_prices = update_prices_ozon(article_info=article_info)
+                write_price_to_excel(current_prices=current_prices, marketplace='ОЗОН')
+                print('✅ Цены Ozon обновлены и сохранены.')
 
-        case '5':
-            print('Обновление цен Wildberries...')
-            article_info = load_article_info_from_excel(sheet_name='ВБ')
-            current_prices = update_prices_wb(article_info=article_info)
-            write_price_to_excel(current_prices=current_prices, marketplace='ВБ')
-            print('✅ Цены Wildberries обновлены и сохранены.')
+            case '5':
+                print('Обновление цен Wildberries...')
+                article_info = load_article_info_from_excel(sheet_name='ВБ')
+                current_prices = update_prices_wb(article_info=article_info)
+                write_price_to_excel(current_prices=current_prices, marketplace='ВБ')
+                print('✅ Цены Wildberries обновлены и сохранены.')
 
-        case '6':
-            print('Обновление цен Ozon...')
-            article_info_ozon = load_article_info_from_excel(sheet_name='ОЗОН')
-            current_prices_ozon = update_prices_ozon(article_info=article_info_ozon)
-            write_price_to_excel(current_prices=current_prices_ozon, marketplace='ОЗОН')
-            print('✅ Цены Ozon обновлены и сохранены.')
+            case '6':
+                print('Обновление цен Ozon...')
+                article_info_ozon = load_article_info_from_excel(sheet_name='ОЗОН')
+                current_prices_ozon = update_prices_ozon(article_info=article_info_ozon)
+                write_price_to_excel(current_prices=current_prices_ozon, marketplace='ОЗОН')
+                print('✅ Цены Ozon обновлены и сохранены.')
 
-            print('Обновление цен Wildberries...')
-            article_info_wb = load_article_info_from_excel(sheet_name='ВБ')
-            current_prices_wb = update_prices_wb(article_info=article_info_wb)
-            write_price_to_excel(current_prices=current_prices_wb, marketplace='ВБ')
-            print('✅ Цены Wildberries обновлены и сохранены.')
+                print('Обновление цен Wildberries...')
+                article_info_wb = load_article_info_from_excel(sheet_name='ВБ')
+                current_prices_wb = update_prices_wb(article_info=article_info_wb)
+                write_price_to_excel(current_prices=current_prices_wb, marketplace='ВБ')
+                print('✅ Цены Wildberries обновлены и сохранены.')
 
-        case _:
-            print('Введено неправильное значение')
+            case _:
+                print('Введено неправильное значение')
+
+        input("Нажмите Enter, чтобы закрыть программу...")
+
+    except Exception as ex:
+        print(f'main: {ex}')
+        input("Нажмите Enter, чтобы закрыть программу...")
 
     execution_time = datetime.now() - start_time
     print(f'Время работы программы: {execution_time}')
