b0bbd83bf9b9f1fa6ca8f08886c711ebf27272f8
maxbaranov <maxexplorer@mail.ru>
2025-08-14 14:10:30 +0300

    feat (neftemash): add def get_products_data()

--- neftemash-m_site/main.py
+++ neftemash-m_site/main.py
@@ -83,113 +83,114 @@ def get_products_data(product_data_list: list) -> list[dict]:
 
     with Session() as session:
         for category_dict in product_data_list:
-            for category_name, category_list in category_dict.items():
-            try:
-                time.sleep(1)
-                html = get_html(url=product_url, headers=headers, session=session)
-            except Exception as ex:
-                print(f"{product_url} - {ex}")
-                continue
+            for category_name, product_urls in category_dict.items():
+                for product_url in product_urls:
+                    try:
+                        time.sleep(1)
+                        html = get_html(url=product_url, headers=headers, session=session)
+                    except Exception as ex:
+                        print(f"{product_url} - {ex}")
+                        continue
 
-            if not html:
-                continue
+                    if not html:
+                        continue
 
-            soup = BeautifulSoup(html, 'lxml')
+                    soup = BeautifulSoup(html, 'lxml')
 
-            try:
-                title = soup.find('h1').text.strip()
-            except Exception:
-                title = None
+                    try:
+                        title = soup.find('h1').text.strip()
+                    except Exception:
+                        title = None
 
-            try:
-                price = int(
-                    ''.join(filter(lambda x: x.isdigit(), soup.find('span', class_='catalogbox__price').text.strip())))
-            except Exception:
-                price = None
+                    try:
+                        price = int(
+                            ''.join(filter(lambda x: x.isdigit(), soup.find('span', class_='catalogbox__price').text.strip())))
+                    except Exception:
+                        price = None
 
-            try:
-                images_items = soup.find('div', class_='catalogbox__galbox js-catalog-detail-fotorama-wrap').find_all(
-                    'a')
-                for image_item in images_items:
-                    image_url = f"https://bellodeco.ru/{image_item.get('href')}"
-                    images_urls_list.append(image_url)
-                    product_images_urls_list.append(image_url)
-                main_image_url = product_images_urls_list[0]
-                additional_images_urls = '; '.join(product_images_urls_list[1:])
-            except Exception:
-                main_image_url = None
-                additional_images_urls = None
+                    try:
+                        images_items = soup.find('div', class_='catalogbox__galbox js-catalog-detail-fotorama-wrap').find_all(
+                            'a')
+                        for image_item in images_items:
+                            image_url = f"https://bellodeco.ru/{image_item.get('href')}"
+                            images_urls_list.append(image_url)
+                            product_images_urls_list.append(image_url)
+                        main_image_url = product_images_urls_list[0]
+                        additional_images_urls = '; '.join(product_images_urls_list[1:])
+                    except Exception:
+                        main_image_url = None
+                        additional_images_urls = None
 
-            try:
-                description = soup.find('div', class_='catalogbox__description').text.strip().splitlines()[-1]
-            except Exception:
-                description = None
+                    try:
+                        description = soup.find('div', class_='catalogbox__description').text.strip().splitlines()[-1]
+                    except Exception:
+                        description = None
 
-            try:
-                models = soup.find('div', class_='b-model-list').find_all('div', class_='model-list__item')
-                try:
-                    model_fbx = models[0].find('a').get('href')
-                except Exception:
-                    model_fbx = None
-                try:
-                    model_obj = models[1].find('a').get('href')
-                except Exception:
-                    model_obj = None
-                try:
-                    model_3ds = models[2].find('a').get('href')
-                except Exception:
-                    model_3ds = None
-            except Exception:
-                pass
+                    try:
+                        models = soup.find('div', class_='b-model-list').find_all('div', class_='model-list__item')
+                        try:
+                            model_fbx = models[0].find('a').get('href')
+                        except Exception:
+                            model_fbx = None
+                        try:
+                            model_obj = models[1].find('a').get('href')
+                        except Exception:
+                            model_obj = None
+                        try:
+                            model_3ds = models[2].find('a').get('href')
+                        except Exception:
+                            model_3ds = None
+                    except Exception:
+                        pass
 
-            result_dict = {
-                'Название товара': title,
-                'Ссылка': product_url,
-                'Цена': price,
-                'Главное изображение': main_image_url,
-                'Дополнительные изображения': additional_images_urls,
-                'Описание': description,
-                'Модель fbs': model_fbx,
-                'Модель obj': model_obj,
-                'Модель 3ds': model_3ds,
-            }
+                    result_dict = {
+                        'Название товара': title,
+                        'Ссылка': product_url,
+                        'Цена': price,
+                        'Главное изображение': main_image_url,
+                        'Дополнительные изображения': additional_images_urls,
+                        'Описание': description,
+                        'Модель fbs': model_fbx,
+                        'Модель obj': model_obj,
+                        'Модель 3ds': model_3ds,
+                    }
 
-            # Сбор параметров товара
-            product_parameters = {}
-            try:
-                parameters_items = soup.find('div', class_='catalogbox__param__left').find_all('div')
-                for parameter_item in parameters_items:
-                    attribute_name = parameter_item.text.split(':')[0].strip()
-                    attribute_value = parameter_item.text.split(':')[1].strip()
-                    product_parameters[attribute_name] = attribute_value
-            except Exception:
-                pass
+                    # Сбор параметров товара
+                    product_parameters = {}
+                    try:
+                        parameters_items = soup.find('div', class_='catalogbox__param__left').find_all('div')
+                        for parameter_item in parameters_items:
+                            attribute_name = parameter_item.text.split(':')[0].strip()
+                            attribute_value = parameter_item.text.split(':')[1].strip()
+                            product_parameters[attribute_name] = attribute_value
+                    except Exception:
+                        pass
 
-            # Сбор характеристик товара
-            product_characteristics = {}
-            try:
-                characteristics_items = soup.find('div', id='tabs-obj_description').find_all('tr')
-                for characteristic_item in characteristics_items:
-                    characteristic = characteristic_item.find_all('td')
-                    attribute_name = characteristic[0].text.strip()
-                    attribute_value = characteristic[1].text.strip()
-                    product_characteristics[attribute_name] = attribute_value
-            except Exception:
-                pass
+                    # Сбор характеристик товара
+                    product_characteristics = {}
+                    try:
+                        characteristics_items = soup.find('div', id='tabs-obj_description').find_all('tr')
+                        for characteristic_item in characteristics_items:
+                            characteristic = characteristic_item.find_all('td')
+                            attribute_name = characteristic[0].text.strip()
+                            attribute_value = characteristic[1].text.strip()
+                            product_characteristics[attribute_name] = attribute_value
+                    except Exception:
+                        pass
 
-            product_attributes = {**product_parameters, **product_characteristics}
+                    product_attributes = {**product_parameters, **product_characteristics}
 
-            result_dict.update(product_attributes)
+                    result_dict.update(product_attributes)
 
-            result_data.append(result_dict)
+                    result_data.append(result_dict)
 
-            print(f'Обработано: {j}/{count_urls}')
+                    print(f'Обработано: {j}/{count_urls}')
 
-    if not os.path.exists('data'):
-        os.mkdir('data')
+        if not os.path.exists('data'):
+            os.mkdir('data')
 
-    with open('data/images_urls_list.txt', 'w', encoding='utf-8') as file:
-        print(*images_urls_list, file=file, sep='\n')
+        with open('data/images_urls_list.txt', 'w', encoding='utf-8') as file:
+            print(*images_urls_list, file=file, sep='\n')
 
     return result_data
 
