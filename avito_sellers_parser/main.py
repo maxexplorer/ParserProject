66f60b6d4171afcb10b384e187e9426b20029251
maxbaranov <maxexplorer@mail.ru>
2025-09-24 18:34:55 +0300

    complete (avito_sellers_parser)

--- avito_sellers_parser/main.py
+++ avito_sellers_parser/main.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from urllib.parse import urlparse, parse_qs
 
 from requests import Session
-
+from pandas import DataFrame, ExcelWriter, read_excel
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
 
@@ -32,19 +32,70 @@ def get_normalize_article(article: str) -> str | None:
     return article.strip()
 
 
-def get_products_data(seller_url: str, limit: int = 100, own: bool = False) -> dict:
+def read_excel_data(file_path: str, own: bool = False) -> dict:
+    """
+    Чтение данных из Excel файла Avito.
+    own=True → возвращает {артикул: цена}
+    own=False → возвращает {артикул: {'Название', 'Бренд', 'Цена', 'Ссылка'}}
+    """
+    df = read_excel(file_path, header=0, sheet_name=0)  # считаем заголовки из первой строки
+    data_dict = {}
+
+    if own:
+        # Ожидаем колонки: ['Артикул', 'Наша цена']
+        for _, row in df.iterrows():
+            article = str(row[df.columns[0]]).replace('-', '').strip()
+            price = row[df.columns[1]]
+            if article:
+                data_dict[article] = price
+    else:
+        # Ожидаем колонки: ['Артикул', 'Название', 'Бренд', 'Цена', 'Ссылка']
+        for _, row in df.iterrows():
+            article = str(row[df.columns[1]]).replace('-', '').strip()
+            title = row.get('Название')
+            brand = row.get('Бренд')
+            price = row.get('Цена')
+            url = row.get('Ссылка')
+            if article:
+                data_dict[article] = {
+                    "Название": title,
+                    "Бренд": brand,
+                    "Цена": price,
+                    "Ссылка": url
+                }
+
+    return data_dict
+
+
+# Функция для записи данных в формат xlsx
+def save_excel(data: list, species: str) -> None:
+    directory = 'results'
+
+    os.makedirs(directory, exist_ok=True)
+
+    file_path = f'{directory}/result_data_{species}.xlsx'
+
+    dataframe = DataFrame(data)
+
+    with ExcelWriter(file_path, mode='w') as writer:
+        dataframe.to_excel(writer, sheet_name='data', index=False)
+
+    print(f'Данные сохранены в файл {file_path}')
+
+
+def get_products_data(seller_url: str, limit: int = 100, own: bool = False, species: str = None):
     """
     Сбор данных по Avito.
-    Если own=True → вернёт {артикул: цена}
-    Если own=False → вернёт {артикул: {'Название':..., 'Бренд':..., 'Цена':..., 'Ссылка':...}}
+    Если own=True → сохраняем {артикул: цена} в own_result.xlsx
+    Если own=False → сохраняем {артикул: {...}} в competitor_data.xlsx
     """
     seller_id = get_seller_id(seller_url)
     if not seller_id:
         raise ValueError("Не удалось извлечь sellerId")
 
-    result_dict = {}
-
     with Session() as session:
+        result_list = []
+
         headers = {
             'accept': 'application/json',
             'referer': seller_url,
@@ -66,7 +117,7 @@ def get_products_data(seller_url: str, limit: int = 100, own: bool = False) -> d
         total = json_data.get('foundCount', 0)
         if total == 0:
             print(f"{seller_url}: товаров нет")
-            return result_dict
+            return
 
         pages = math.ceil(total / limit)
         print(f"{seller_url}: всего {total} товаров, {pages} страниц")
@@ -101,45 +152,53 @@ def get_products_data(seller_url: str, limit: int = 100, own: bool = False) -> d
 
             for item in items:
                 product_url = f"https://www.avito.ru{item.get('urlPath')}"
-
                 title = item.get('title')
 
                 article_list = item.get('iva', {}).get('SparePartsParamsStep') or []
                 article = article_list[0].get('payload', {}).get('text') if article_list else None
                 normalize_article = get_normalize_article(article)
                 if not normalize_article:
-                    print(f'not article: {product_url}')
                     continue
 
                 prices = item.get('priceDetailed')
                 if not prices:
-                    print(f'not prices: {product_url}')
                     continue
                 price = prices.get('value')
 
                 if own:
-                    result_dict[normalize_article] = price
+                    result_list.append(
+                        {
+                            'Артикул': normalize_article,
+                            'Цена': price,
+                        }
+                    )
                 else:
                     brand_list = item.get('iva', {}).get('AutoPartsManufacturerStep') or []
                     brand = brand_list[0].get('payload', {}).get('value') if brand_list else None
-                    result_dict[normalize_article] = {
-                        'Название': title,
-                        'Бренд': brand,
-                        'Цена': price,
-                        'Ссылка': product_url
-                    }
+                    result_list.append(
+                        {
+                            'Название': title,
+                            'Артикул': normalize_article,
+                            'Бренд': brand,
+                            'Цена': price,
+                            'Ссылка': product_url
+                        }
+                    )
 
             print(f"Обработано страниц: {page}/{pages}")
 
-        print(f"{seller_url}: обработано {len(result_dict)} записей")
+        print(f"{seller_url}: обработано {len(result_list)} записей")
 
-    return result_dict
+    save_excel(result_list, species)
 
 
-def process_data_files(own_dict: dict, competitor_dict: dict, data_folder='data'):
+def process_data_files(data_folder='data'):
+    # Загружаем ранее сохранённые Excel
+    own_dict = read_excel_data('results/result_data_own.xlsx', own=True)
+    competitor_dict = read_excel_data('results/result_data_competitor.xlsx', own=False)
+
     excel_files = glob.glob(os.path.join(data_folder, "*.xls*"))
 
-    # цвета для подсветки
     green_fill = PatternFill(start_color="C6EFCE", end_color="C6EFCE", fill_type="solid")
     red_fill = PatternFill(start_color="FFC7CE", end_color="FFC7CE", fill_type="solid")
     yellow_fill = PatternFill(start_color="FFEB9C", end_color="FFEB9C", fill_type="solid")
@@ -158,7 +217,6 @@ def process_data_files(own_dict: dict, competitor_dict: dict, data_folder='data'
             ws = wb[sheet_name]
             headers = [cell.value for cell in ws[2]]
 
-            # Добавляем колонки если отсутствуют
             needed_cols = ['Бренд', 'Цена', 'Ссылка', 'Наша цена']
             col_indices = {}
             for col in needed_cols:
@@ -167,7 +225,6 @@ def process_data_files(own_dict: dict, competitor_dict: dict, data_folder='data'
                     headers.append(col)
                 col_indices[col] = headers.index(col) + 1
 
-            # Проход по строкам
             for row in ws.iter_rows(min_row=3, max_col=len(headers)):
                 article_cell = row[1].value
                 if not article_cell:
@@ -185,7 +242,6 @@ def process_data_files(own_dict: dict, competitor_dict: dict, data_folder='data'
                     ws.cell(row=row[0].row, column=col_indices['Цена'], value=competitor_data['Цена'])
                     ws.cell(row=row[0].row, column=col_indices['Ссылка'], value=competitor_data['Ссылка'])
 
-                    # Подсветка цены конкурента
                     if own_price and competitor_data['Цена']:
                         if own_price < competitor_data['Цена']:
                             ws.cell(row=row[0].row, column=col_indices['Цена']).fill = green_fill
@@ -199,18 +255,15 @@ def process_data_files(own_dict: dict, competitor_dict: dict, data_folder='data'
 
 
 def main():
-    # 1. Собираем данные своего магазина
-    own_url = "https://www.avito.ru/brands/gg_auto/all/zapchasti_i_aksessuary?sellerId=de08ae472d1d705873cca3d2970af199"
-    print("[INFO] Сбор данных своего магазина...")
-    own_dict = get_products_data(seller_url=own_url, own=True)
+    # own_url = "https://www.avito.ru/brands/gg_auto/all/zapchasti_i_aksessuary?sellerId=de08ae472d1d705873cca3d2970af199"
+    # print("[INFO] Сбор данных своего магазина...")
+    # get_products_data(seller_url=own_url, own=True, species='own')
+    #
+    # competitor_url = input("Введите ссылку на магазин конкурента: \n").strip()
+    # print("[INFO] Сбор данных конкурента...")
+    # get_products_data(seller_url=competitor_url, own=False, species='competitor')
 
-    # 2. Вводим конкурента
-    competitor_url = input("Введите ссылку на магазин конкурента: \n").strip()
-    print("[INFO] Сбор данных конкурента...")
-    competitor_dict = get_products_data(seller_url=competitor_url, own=False)
-
-    # 3. Обновляем таблицу
-    process_data_files(own_dict=own_dict, competitor_dict=competitor_dict)
+    process_data_files()
 
     execution_time = datetime.now() - start_time
     print('Сбор данных завершен.')
