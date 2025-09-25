25eb2e2bb36a7a9014e2317fff4de46ad74a67fc
maxbaranov <maxexplorer@mail.ru>
2025-09-25 14:06:54 +0300

    complete (elbrus_b2b)

--- elbrus_b2b_site/main.py
+++ elbrus_b2b_site/main.py
@@ -1,18 +1,39 @@
 import os
 import time
-import math
 import glob
 import re
 from datetime import datetime
-from urllib.parse import urlparse, parse_qs
+from random import randint
 
-from requests import Session, session
-from pandas import DataFrame, ExcelWriter, read_excel
+from requests import Session
+from pandas import DataFrame, ExcelWriter
 from openpyxl import load_workbook
-from openpyxl.styles import PatternFill
+
+from bs4 import BeautifulSoup
 
 start_time = datetime.now()
 
+
+def get_html(url: str, headers: dict, session: Session) -> str | None:
+    """
+    Выполняет GET-запрос по указанному URL и возвращает HTML-код страницы.
+
+    :param url: Ссылка на страницу
+    :param headers: Заголовки запроса (user-agent и др.)
+    :param session: Сессия requests для повторного использования соединений
+    :return: HTML содержимое страницы или None в случае ошибки
+    """
+    try:
+        response = session.get(url=url, headers=headers, timeout=60)
+
+        if response.status_code != 200:
+            print(f'status_code: {response.status_code}')
+
+        html = response.text
+        return html
+    except Exception as ex:
+        print(f'get_html: {ex}')
+        return None
 
 
 def get_normalize_article(article) -> str | None:
@@ -32,6 +53,13 @@ def get_normalize_article(article) -> str | None:
     return article.strip() or None
 
 
+def extract_value(soup: BeautifulSoup, label: str) -> float | None:
+    try:
+        value_text = soup.find('span', string=re.compile(label)).find_next().get_text(strip=True)
+        return float(value_text.replace(',', '.')) / 1000  # на случай запятой
+    except Exception:
+        return None
+
 
 # Функция для записи данных в формат xlsx
 def save_excel(data: list, species: str) -> None:
@@ -49,151 +77,110 @@ def save_excel(data: list, species: str) -> None:
     print(f'Данные сохранены в файл {file_path}')
 
 
-def get_products_data(seller_url: str, limit: int = 100, own: bool = False, species: str = None):
-    """
-    Сбор данных по Avito.
-    Если own=True → сохраняем {артикул: цена} в own_result.xlsx
-    Если own=False → сохраняем {артикул: {...}} в competitor_data.xlsx
-    """
-    seller_id = get_seller_id(seller_url)
-    if not seller_id:
-        raise ValueError("Не удалось извлечь sellerId")
+def get_product_data(article: str, headers: dict, session: Session, ):
+    product_url = f"https://elbrus-b2b.ru/search?pcode={article}"
 
-    with Session() as session:
-        result_list = []
+    try:
+        time.sleep(randint(1, 3))
+        html = get_html(url=product_url, headers=headers, session=session)
+    except Exception as ex:
+        html = None
+        print(f'get_products_urls: {product_url} - {ex}')
 
-        headers = {
-            'accept': 'application/json',
-            'referer': seller_url,
-            'user-agent': 'Mozilla/5.0',
-            'x-requested-with': 'XMLHttpRequest',
-        }
+    if not html:
+        print(f'not html product_url: {product_url}')
 
-        first_params = {
-            'p': 1,
-            'sellerId': seller_id,
-            'itemsOnPage': limit,
-            'limit': limit}
+    soup = BeautifulSoup(html, 'lxml')
 
-        response = session.get('https://www.avito.ru/web/1/profile/items',
-                               params=first_params, headers=headers, timeout=(3, 5))
-        response.raise_for_status()
-        json_data = response.json()
+    try:
+        short_url = soup.find('div', class_='searchTopLinks').find_next_sibling('a').get('href')
+        review_url = f'https://elbrus-b2b.ru{short_url}'
+    except Exception as ex:
+        review_url = None
+        print(f'not review_url: {product_url} - {ex}')
 
-        total = json_data.get('foundCount', 0)
-        if total == 0:
-            print(f"{seller_url}: товаров нет")
-            return
+    try:
+        time.sleep(randint(1, 3))
+        html = get_html(url=review_url, headers=headers, session=session)
+    except Exception as ex:
+        html = None
+        print(f'get_products_urls: {product_url} - {ex}')
 
-        pages = math.ceil(total / limit)
-        print(f"{seller_url}: всего {total} товаров, {pages} страниц")
+    if not html:
+        print(f'not html review_url: {product_url}')
 
-        # цикл по страницам
-        for page in range(1, pages + 1):
-            params = first_params.copy()
-            params['p'] = page
+    soup = BeautifulSoup(html, 'lxml')
 
-            try:
-                time.sleep(1)
-                response = session.get(
-                    'https://www.avito.ru/web/1/profile/items',
-                    params=params,
-                    headers=headers,
-                    timeout=(3, 5)
-                )
+    height_m = extract_value(soup, 'Высота упаковки, мм:')
 
-                if response.status_code != 200:
-                    print(f' seller_url: {seller_url}: статус ответа {response.status_code}')
-                    continue
+    length_m = extract_value(soup, 'Длина упаковки, мм:')
 
-                json_data: dict = response.json()
-                items: list = json_data.get('catalog', {}).get('items', [])
+    width_m = extract_value(soup, 'Ширина упаковки, мм:')
 
-            except Exception as ex:
-                print(f"{seller_url} страница {page}: {ex}")
-                continue
+    if None in (height_m, length_m, width_m):
+        return None
 
-            if not items:
-                continue
+    volume_m3 = height_m * length_m * width_m
 
-            for item in items:
-                product_url = f"https://www.avito.ru{item.get('urlPath')}"
-                title = item.get('title')
-
-                article_list = item.get('iva', {}).get('SparePartsParamsStep') or []
-                article = article_list[0].get('payload', {}).get('text') if article_list else None
-                normalize_article = get_normalize_article(article)
-                if not normalize_article:
-                    continue
-
-                prices = item.get('priceDetailed')
-                if not prices:
-                    continue
-                price = prices.get('value')
-
-                if own:
-                    result_list.append(
-                        {
-                            'Артикул': normalize_article,
-                            'Цена': price,
-                        }
-                    )
-                else:
-                    brand_list = item.get('iva', {}).get('AutoPartsManufacturerStep') or []
-                    brand = brand_list[0].get('payload', {}).get('value') if brand_list else None
-                    result_list.append(
-                        {
-                            'Название': title,
-                            'Артикул': normalize_article,
-                            'Бренд': brand,
-                            'Цена': price,
-                            'Ссылка': product_url
-                        }
-                    )
-
-            print(f"Обработано страниц: {page}/{pages}")
-
-        print(f"{seller_url}: обработано {len(result_list)} записей")
-
-    save_excel(result_list, species)
+    return volume_m3
 
 
 def process_data_files(data_folder='data'):
-
     excel_files = glob.glob(os.path.join(data_folder, "*.xls*"))
 
-    for data_file_path in excel_files:
-        file_name = os.path.basename(data_file_path)
-        print(f'[INFO] Обрабатывается файл: {file_name}')
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+        'priority': 'u=0, i',
+        'referer': 'https://elbrus-b2b.ru/search/O.E.M./OEM0003KBPL',
+        'sec-ch-ua': '"Chromium";v="140", "Not=A?Brand";v="24", "Google Chrome";v="140"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
+    }
 
-        try:
-            wb = load_workbook(data_file_path)
-        except Exception as e:
-            print(f'[ERROR] Ошибка чтения файла {file_name}: {e}')
-            continue
+    with Session() as session:
+        for data_file_path in excel_files:
+            file_name = os.path.basename(data_file_path)
+            print(f'[INFO] Обрабатывается файл: {file_name}')
 
-        for sheet_name in wb.sheetnames:
-            ws = wb[sheet_name]
-            headers = [cell.value for cell in ws[4]]
+            try:
+                wb = load_workbook(data_file_path)
+            except Exception as e:
+                print(f'[ERROR] Ошибка чтения файла {file_name}: {e}')
+                continue
 
-            volume_column_index = headers.index('Volume')  # Ищем столбец 'Volume'
+            for sheet_name in wb.sheetnames:
+                ws = wb[sheet_name]
+                headers_excel = [cell.value for cell in ws[4]]
 
-            for row in ws.iter_rows(min_row=5, max_col=len(headers)):
-                article_cell = row[1].value
-                if not article_cell:
-                    continue
-                article = get_normalize_article(article_cell)
+                volume_column_index = headers_excel.index('Volume')  # Ищем столбец 'Volume'
 
-                volume = get_products_data(url=url, headers=headers, session=session)
+                for row in ws.iter_rows(min_row=5, max_col=len(headers)):
+                    article_cell = row[1].value
+                    if not article_cell:
+                        continue
+                    article = get_normalize_article(article_cell)
 
-                row[volume_column_index].value = volume
+                    volume_m3 = get_product_data(article=article, headers=headers, session=session)
 
-        wb.save(data_file_path)
-        print(f"[INFO] Файл обновлён: {file_name}")
+                    if not volume_m3:
+                        continue
+
+                    row[volume_column_index].value = volume_m3
+
+                    print(f'Обработан артикул: {article} ')
+
+                wb.save(data_file_path)  # сохраняем после обработки текущего листа
+                print(f"[INFO] Лист '{sheet_name}' сохранён")
 
 
 def main():
-
     process_data_files()
 
     execution_time = datetime.now() - start_time
