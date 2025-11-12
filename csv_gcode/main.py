6deeced0b33d4e5c6c2e15ec247bdd1a8884bccb
maxbaranov <maxexplorer@mail.ru>
2025-11-12 16:53:15 +0300

    complete (csv_gcode)

--- csv_gcode/main.py
+++ csv_gcode/main.py
@@ -1,14 +1,31 @@
 # main.py
 
 import os
+from datetime import datetime
 
 from processor import process_all_csv
 
-def main():
-    input_folder = os.path.join(os.getcwd(), 'csv_data')
-    output_folder = os.path.join(os.getcwd(), 'results')
 
-    process_all_csv(input_folder, output_folder)
+def main():
+    """Основная точка входа в программу"""
+    start_time = datetime.now()
+
+    try:
+        input_folder = os.path.join(os.getcwd(), 'csv_data')
+        output_folder = os.path.join(os.getcwd(), 'results')
+
+        process_all_csv(input_folder, output_folder)
+
+    except Exception as ex:
+        print(f'main: {ex}')
+        input("Нажмите Enter, чтобы закрыть программу...")
+
+    execution_time = datetime.now() - start_time
+    print('Сбор данных завершен!')
+    print(f'Время работы программы: {execution_time}')
+
+    input("Нажмите Enter, чтобы закрыть программу...")
+
 
 if __name__ == '__main__':
     main()
